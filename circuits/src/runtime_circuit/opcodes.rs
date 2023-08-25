c16615bb36040830263b60939a2833d3176ef1fb
Dmitry Savonin <nwtsoas@gmail.com>
2023-08-25 23:38:48 +0300

    added several region assignments

--- circuits/src/runtime_circuit/opcodes.rs
+++ circuits/src/runtime_circuit/opcodes.rs
@@ -3,6 +3,7 @@ mod op_drop;
 mod op_local;
 
 use crate::{runtime_circuit::constraint_builder::OpConstraintBuilder, util::Field};
+use fluentbase_rwasm::engine::bytecode::Instruction;
 use halo2_proofs::{circuit::Region, plonk::Error};
 
 pub(crate) trait ExecutionGadget<F: Field> {
@@ -10,5 +11,17 @@ pub(crate) trait ExecutionGadget<F: Field> {
 
     fn configure(cb: &mut OpConstraintBuilder<F>) -> Self;
 
-    fn assign_exec_step(&self, region: &mut Region<'_, F>, offset: usize) -> Result<(), Error>;
+    fn assign_exec_step(
+        &self,
+        region: &mut Region<'_, F>,
+        offset: usize,
+        instr: Instruction,
+    ) -> Result<(), Error>;
+}
+
+#[macro_export]
+macro_rules! bail_illegal_opcode {
+    ($instr:expr) => {
+        unreachable!("illegal opcode {:?}", $instr)
+    };
 }
