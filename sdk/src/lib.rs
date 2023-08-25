7f521000f2711f1d9c2162cf7476a68462eb9641
Dmitry Savonin <nwtsoas@gmail.com>
2023-08-25 23:44:39 +0300

    moved core to sdk crate and core is reserved for common components

---
+++ sdk/src/lib.rs
@@ -0,0 +1,30 @@
+#![feature(lang_items)]
+#![no_std]
+
+#[cfg(not(feature = "std"))]
+extern crate wee_alloc;
+
+#[cfg(not(feature = "std"))]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[cfg(not(feature = "std"))]
+#[panic_handler]
+#[inline(always)]
+fn panic(info: &core::panic::PanicInfo) -> ! {
+    if let Some(panic_message) = info.payload().downcast_ref::<&str>() {
+        evm_return_raw(panic_message.as_ptr(), panic_message.len() as u32);
+    }
+    sys_panic();
+    loop {}
+}
+
+mod binding;
+pub use binding::*;
+
+const HALT_CODE_EXIT: u32 = 0;
+const HALT_CODE_PANIC: u32 = 1;
+
+#[cfg(not(feature = "std"))]
+#[lang = "eh_personality"]
+extern "C" fn eh_personality() {}
