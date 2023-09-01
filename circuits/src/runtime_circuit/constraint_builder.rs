214366c7a0c2cebbe847669e5cbc5383027d7b3f
Dmitry Savonin <nwtsoas@gmail.com>
2023-09-01 18:23:43 +0300

    fixed rw counter and stack pointer calculation

--- circuits/src/runtime_circuit/constraint_builder.rs
+++ circuits/src/runtime_circuit/constraint_builder.rs
@@ -17,7 +17,6 @@ use crate::{
 };
 use fluentbase_rwasm::engine::bytecode::Instruction;
 use halo2_proofs::{circuit::Region, plonk::ConstraintSystem};
-use std::ops::{Add, Sub};
 
 #[derive(Clone)]
 pub struct StateTransition<F: Field> {
@@ -47,7 +46,7 @@ impl<F: Field> StateTransition<F> {
         rw_counter: u64,
     ) {
         self.stack_pointer.assign(region, offset, stack_pointer);
-        self.stack_pointer.assign(region, offset, rw_counter);
+        self.rw_counter.assign(region, offset, rw_counter);
     }
 
     pub fn stack_pointer(&self) -> Query<F> {
@@ -126,24 +125,20 @@ impl<'cs, 'st, F: Field> OpConstraintBuilder<'cs, 'st, F> {
         self.base.advice_column_phase2(self.cs)
     }
 
-    pub fn stack_lookup(&mut self, is_write: Query<F>, address: Query<F>, value: Query<F>) {
-        self.rw_lookup(is_write, RwTableTag::Stack.expr(), address, value);
-    }
-
     pub fn stack_push(&mut self, value: Query<F>) {
-        self.stack_lookup(Query::one(), self.state_transition.stack_pointer(), value);
         self.state_transition.stack_pointer_offset =
-            self.state_transition.stack_pointer_offset.clone().sub(1);
+            self.state_transition.stack_pointer_offset.clone() - self.base.resolve_condition().0;
+        self.stack_lookup(Query::one(), self.state_transition.stack_pointer(), value);
     }
 
     pub fn stack_pop(&mut self, value: Query<F>) {
-        self.state_transition.stack_pointer_offset =
-            self.state_transition.stack_pointer_offset.clone().add(1);
         self.stack_lookup(Query::zero(), self.state_transition.stack_pointer(), value);
+        self.state_transition.stack_pointer_offset =
+            self.state_transition.stack_pointer_offset.clone() + self.base.resolve_condition().0;
     }
 
-    pub fn global_lookup(&mut self, is_write: Query<F>, address: Query<F>, value: Query<F>) {
-        self.rw_lookup(is_write, RwTableTag::Global.expr(), address, value);
+    pub fn stack_lookup(&mut self, is_write: Query<F>, address: Query<F>, value: Query<F>) {
+        self.rw_lookup(is_write, RwTableTag::Stack.expr(), address, value);
     }
 
     pub fn global_get(&mut self, index: Query<F>, value: Query<F>) {
@@ -152,6 +147,10 @@ impl<'cs, 'st, F: Field> OpConstraintBuilder<'cs, 'st, F> {
 
     pub fn global_set(&mut self, index: Query<F>, value: Query<F>) {
         self.global_lookup(Query::one(), index, value);
+    }
+
+    pub fn global_lookup(&mut self, is_write: Query<F>, address: Query<F>, value: Query<F>) {
+        self.rw_lookup(is_write, RwTableTag::Global.expr(), address, value);
     }
 
     pub fn execution_state_lookup(&mut self, execution_state: ExecutionState, opcode: Query<F>) {
@@ -178,6 +177,12 @@ impl<'cs, 'st, F: Field> OpConstraintBuilder<'cs, 'st, F> {
         address: Query<F>,
         value: Query<F>,
     ) {
+        // println!(
+        //     "rw_lookup: rw_counter={:?}, is_write={:?}, tag={:?}",
+        //     self.state_transition.rw_counter(),
+        //     is_write,
+        //     tag
+        // );
         self.op_lookups
             .push(LookupTable::Rw(self.base.apply_lookup_condition([
                 Query::one(),
@@ -188,11 +193,8 @@ impl<'cs, 'st, F: Field> OpConstraintBuilder<'cs, 'st, F> {
                 address,
                 value,
             ])));
-        let condition = self.base.resolve_condition();
-        // self.base.condition(condition, |cb| {
-        // });
         self.state_transition.rw_counter_offset =
-            self.state_transition.rw_counter_offset.clone() + 1;
+            self.base.resolve_condition().0 * self.state_transition.rw_counter_offset.clone() + 1;
     }
 
     pub fn stack_pointer_offset(&self) -> Query<F> {
@@ -204,7 +206,11 @@ impl<'cs, 'st, F: Field> OpConstraintBuilder<'cs, 'st, F> {
     }
 
     pub fn require_opcode(&mut self, instr: Instruction) {
-        self.require_equal("opcode", self.opcode.current(), instr.code_value().expr());
+        self.require_equal(
+            "opcode matches specific instr",
+            self.opcode.current(),
+            instr.code_value().expr(),
+        );
     }
 
     pub fn condition(&mut self, condition: Query<F>, configure: impl FnOnce(&mut Self)) {
