a7070a72bfe9d2b19b315923da154b331ac25d82
Stas Stepanov <stanislav@fluentlabs.xyz>
2023-10-11 16:23:15 -0500

    fix zktrie + added test

--- runtime/src/tests.rs
+++ runtime/src/tests.rs
@@ -1,4 +1,4 @@
-use crate::{runtime::Runtime, Error};
+use crate::{instruction::sys_write, runtime::Runtime, Error, HASH_SCHEME_DONE};
 use fluentbase_rwasm::{
     common::Trap,
     rwasm::{Compiler, ImportLinker},
@@ -57,6 +57,42 @@ fn test_greeting() {
     )
     .unwrap();
     assert_eq!(output.data().output().clone(), vec![0, 0, 0, 123]);
+}
+
+#[test]
+fn zktrie_open_test() {
+    use HASH_SCHEME_DONE;
+    assert_eq!(*HASH_SCHEME_DONE, true);
+
+    let wasm_binary = include_bytes!("../examples/bin/zktrie_open_test.wasm");
+    let import_linker = Runtime::new_linker();
+    let rwasm_binary = wasm2rwasm(wasm_binary, &import_linker);
+
+    let mut input_data = vec![];
+
+    let root_updated: Vec<u8> = vec![
+        0x23, 0x36, 0x5e, 0xbd, 0x71, 0xa7, 0xad, 0x35, 0x65, 0xdd, 0x24, 0x88, 0x47, 0xca, 0xe8,
+        0xe8, 0x8, 0x21, 0x15, 0x62, 0xc6, 0x83, 0xdb, 0x8, 0x4f, 0x5a, 0xfb, 0xd1, 0xb0, 0x3d,
+        0x4c, 0xb5,
+    ];
+    input_data.extend(root_updated);
+
+    let key = "key".as_bytes();
+    let mut key_bytes = [0u8; 20];
+    let l = key_bytes.len();
+    key_bytes[l - key.len()..].copy_from_slice(key);
+    input_data.extend(key_bytes.as_slice());
+
+    let mut account_data = [0u8; 32 * 5];
+    account_data[0] = 1;
+    input_data.extend(account_data.as_slice());
+
+    println!("input_data: {:?} len {}", input_data, input_data.len());
+
+    let output =
+        Runtime::run_with_linker(rwasm_binary.as_slice(), &input_data, &import_linker, false)
+            .unwrap();
+    assert_eq!(output.data().output().clone(), vec![]);
 }
 
 fn assert_trap_i32_exit<T>(result: Result<T, Error>, trap_code: Trap) {
