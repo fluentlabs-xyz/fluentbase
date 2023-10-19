e8921e81f234275c13e4695577c011cbaef0a3f8
Eveniizh <eugene@fluentlabs.xyz>
2023-10-19 21:01:53 +0700

    Wasm table, exit code in table get

--- circuits/src/runtime_circuit/opcodes/op_table_get.rs
+++ circuits/src/runtime_circuit/opcodes/op_table_get.rs
@@ -8,18 +8,25 @@ use crate::{
     },
     rw_builder::rw_row::RwTableContextTag,
     util::Field,
+    gadgets::lt::LtGadget,
+    exec_step::MAX_TABLE_SIZE,
 };
 use fluentbase_rwasm::engine::bytecode::Instruction;
 use halo2_proofs::circuit::Region;
 use std::marker::PhantomData;
+use fluentbase_runtime::ExitCode;
 
 #[derive(Clone, Debug)]
 pub(crate) struct OpTableGetGadget<F: Field> {
     elem_index: AdviceColumn,
     value: AdviceColumn,
     size: AdviceColumn,
+    exit_code: AdviceColumn,
+    lt_gadget: LtGadget<F, 2>,
     _pd: PhantomData<F>,
 }
+
+const RANGE_THRESHOLD: usize = MAX_TABLE_SIZE * 2 + 1;
 
 impl<F: Field> ExecutionGadget<F> for OpTableGetGadget<F> {
     const NAME: &'static str = "WASM_TABLE_GET";
@@ -30,18 +37,48 @@ impl<F: Field> ExecutionGadget<F> for OpTableGetGadget<F> {
         let elem_index = cb.query_cell();
         let value = cb.query_cell();
         let size = cb.query_cell();
-        cb.require_opcode(Instruction::TableGet(Default::default()));
+        let exit_code = cb.query_cell();
+
+        cb.range_check_1024(elem_index.expr());
+        cb.range_check_1024(size.expr());
+
+        let range = size.current() - elem_index.current() - 1.expr();
+        // 1024 - 1024 - 1 + 2049 is minimum value.
+        // 1 or more is valid + 2048, so less than 2049 in error.
+        // less than 2049 is out of valid range, so we checking it.
+        // 2049 is RANGE_THRESHOLD now.
+        let threshold = range + RANGE_THRESHOLD.expr();
+        let lt_gadget = cb.lt_gadget(threshold, RANGE_THRESHOLD.expr());
+
+        // So in case of exit code causing error, pc delta must be different.
+        // To solve this problem `configure_state_transition` is defined with disabled constraint.
+        cb.condition(1.expr() - lt_gadget.expr(), |cb| {
+            cb.next_pc_delta(9.expr());
+        });
+
+        cb.condition(lt_gadget.expr(), |cb| {
+            cb.require_zero("exit code must be valid", exit_code.current() - (ExitCode::TableOutOfBounds as i32 as u64).expr());
+        });
+
+        cb.exit_code_lookup(exit_code.current());
 
         cb.stack_pop(elem_index.current());
-        cb.table_elem_lookup(
-            0.expr(),
-            cb.query_rwasm_value(),
-            elem_index.current(),
-            value.current(),
-        );
-        cb.stack_push(value.current());
-        cb.range_check_1024(elem_index.expr());
-        cb.range_check_1024(size.expr() - elem_index.expr());
+
+        cb.condition(1.expr() - lt_gadget.expr(), |cb| {
+            cb.table_elem_lookup(
+                0.expr(),
+                cb.query_rwasm_value(),
+                elem_index.current(),
+                value.current(),
+            );
+            cb.stack_push(value.current());
+        });
+
+        // If exit code causing error than nothing is written, but we need to shift.
+        cb.condition(lt_gadget.expr(), |cb| {
+            cb.draft_shift(2, 0);
+        });
+
         cb.context_lookup(
             RwTableContextTag::TableSize(cb.query_rwasm_value()),
             0.expr(),
@@ -52,8 +89,14 @@ impl<F: Field> ExecutionGadget<F> for OpTableGetGadget<F> {
             elem_index,
             value,
             size,
+            exit_code,
+            lt_gadget,
             _pd: Default::default(),
         }
+    }
+
+    fn configure_state_transition(cb: &mut OpConstraintBuilder<F>) {
+        //cb.next_pc_delta(9.expr());
     }
 
     fn assign_exec_step(
@@ -70,9 +113,21 @@ impl<F: Field> ExecutionGadget<F> for OpTableGetGadget<F> {
             .assign(region, offset, F::from(elem_index.to_bits()));
         self.value.assign(region, offset, F::from(value.to_bits()));
         self.size.assign(region, offset, F::from(size as u64));
+        if elem_index.to_bits() >= size as u64 {
+            let exit_code = ExitCode::TableOutOfBounds as i32 as u64;
+            println!("DEBUG exit_code {}", exit_code);
+            self.exit_code.assign(region, offset, F::from(exit_code));
+        }
+        self.lt_gadget.assign(
+            region,
+            offset,
+            F::from((size as i64 - elem_index.to_bits() as i64 - 1 + RANGE_THRESHOLD as i64) as u64),
+            F::from(RANGE_THRESHOLD as u64),
+        );
         Ok(())
     }
 }
+
 
 #[cfg(test)]
 mod test {
@@ -80,7 +135,7 @@ mod test {
     use fluentbase_rwasm::instruction_set;
 
     #[test]
-    fn table_get() {
+    fn table_get_simple() {
         test_ok(instruction_set! {
             RefFunc(0)
             I32Const(2)
@@ -94,4 +149,21 @@ mod test {
             Drop
         });
     }
+
+    #[test]
+    fn table_get_out_of_bounds() {
+        test_ok(instruction_set! {
+            RefFunc(0)
+            I32Const(2)
+            TableGrow(0)
+            Drop
+            I32Const(0)
+            RefFunc(0)
+            TableSet(0)
+            I32Const(2)
+            TableGet(0)
+            Drop
+        });
+    }
+
 }
