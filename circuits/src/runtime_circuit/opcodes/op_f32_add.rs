f74ae8bfb582a248c88e770d8103009ececa2934
Eveniizh <eugene@fluentlabs.xyz>
2023-11-06 20:19:04 +0700

    Wasm f32, add op, using lt gadget to check shift out

--- circuits/src/runtime_circuit/opcodes/op_f32_add.rs
+++ circuits/src/runtime_circuit/opcodes/op_f32_add.rs
@@ -1,6 +1,7 @@
 use crate::{
     bail_illegal_opcode,
     constraint_builder::{AdviceColumn, ToExpr},
+    gadgets::lt::LtGadget,
     runtime_circuit::{
         constraint_builder::OpConstraintBuilder,
         execution_state::ExecutionState,
@@ -14,6 +15,7 @@ use fluentbase_rwasm::engine::bytecode::Instruction;
 use halo2_proofs::circuit::Region;
 use std::marker::PhantomData;
 use crate::constraint_builder::SelectorColumn;
+use crate::constraint_builder::Query;
 
 #[derive(Clone, Debug)]
 pub(crate) struct OpF32AddGadget<F: Field> {
@@ -27,9 +29,9 @@ pub(crate) struct OpF32AddGadget<F: Field> {
     lhs_limbs: [AdviceColumn; 3],
     rhs_limbs: [AdviceColumn; 3],
     out_limbs: [AdviceColumn; 3],
-    lhs_exp_shift: AdviceColumn,
-    rhs_exp_shift: AdviceColumn,
-    out_exp_shift: AdviceColumn,
+    pow2: AdviceColumn,
+    // 24 bits to compare for addition, that is simpler than multiplication.
+    lt_gadget: Option<LtGadget<F, 3>>,
     _pd: PhantomData<F>,
 }
 
@@ -53,9 +55,9 @@ impl<F: Field> ExecutionGadget<F> for OpF32AddGadget<F> {
         let rhs_limbs = [cb.query_cell(), cb.query_cell(), cb.query_cell()];
         let out_limbs = [cb.query_cell(), cb.query_cell(), cb.query_cell()];
 
-        let lhs_exp_shift = cb.query_cell();
-        let rhs_exp_shift = cb.query_cell();
-        let out_exp_shift = cb.query_cell();
+        let pow2 = cb.query_cell();
+
+        let lhs_sign_bit: Query<F> = lhs_sign.current().into();
 
         cb.range_check8(lhs_exp_ge.current().select(lhs_exp.current() - rhs_exp.current(), 0.expr()));
 
@@ -69,23 +71,23 @@ impl<F: Field> ExecutionGadget<F> for OpF32AddGadget<F> {
             cb.range_check8(out_limbs[i].current());
         }
 
-        cb.range_check7(lhs_limbs[2].current() - 0x7f.expr());
-        cb.range_check7(rhs_limbs[2].current() - 0x7f.expr());
-        cb.range_check7(out_limbs[2].current() - 0x7f.expr());
+        cb.range_check7(lhs_limbs[2].current() - 0x80.expr());
+        cb.range_check7(rhs_limbs[2].current() - 0x80.expr());
+        cb.range_check7(out_limbs[2].current() - 0x80.expr());
 
         cb.stack_pop(
             rhs_sign.current().select(0x80000000_u32.expr(), 0.expr()) + rhs_exp.current() * 0x800000.expr() +
-            (rhs_limbs[2].current() - 0x7f.expr()) * 0x10000.expr() + rhs_limbs[1].current() * 0x100.expr() + rhs_limbs[0].current()
+            (rhs_limbs[2].current() - 0x80.expr()) * 0x10000.expr() + rhs_limbs[1].current() * 0x100.expr() + rhs_limbs[0].current()
         );
 
         cb.stack_pop(
             lhs_sign.current().select(0x80000000_u32.expr(), 0.expr()) + lhs_exp.current() * 0x800000.expr() +
-            (lhs_limbs[2].current() - 0x7f.expr()) * 0x10000.expr() + lhs_limbs[1].current() * 0x100.expr() + lhs_limbs[0].current()
+            (lhs_limbs[2].current() - 0x80.expr()) * 0x10000.expr() + lhs_limbs[1].current() * 0x100.expr() + lhs_limbs[0].current()
         );
 
         cb.stack_push(
             out_sign.current().select(0x80000000_u32.expr(), 0.expr()) + out_exp.current() * 0x800000.expr() +
-            (out_limbs[2].current() - 0x7f.expr()) * 0x10000.expr() + out_limbs[1].current() * 0x100.expr() + out_limbs[0].current()
+            (out_limbs[2].current() - 0x80.expr()) * 0x10000.expr() + out_limbs[1].current() * 0x100.expr() + out_limbs[0].current()
         );
 
         // Halo field is used to represent nagative values.
@@ -97,8 +99,11 @@ impl<F: Field> ExecutionGadget<F> for OpF32AddGadget<F> {
         let out_mant = || out_sign.current().select(0.expr() - out_mant_abs(), out_mant_abs());
 
         let first_exp = || lhs_exp_ge.current().select(lhs_exp.current(), rhs_exp.current());
-        //let second_exp = || lhs_exp_ge.current().select(lhs_exp.current(), rhs_exp.current());
+        let second_exp = || lhs_exp_ge.current().select(rhs_exp.current(), lhs_exp.current());
         let first_mant = || lhs_exp_ge.current().select(lhs_mant(), rhs_mant());
+        let second_mant = || lhs_exp_ge.current().select(rhs_mant(), lhs_mant());
+
+        let mut opt_lt_gadget = None;
 
         // Same sign case.
         cb.condition(lhs_sign.current().xnor(rhs_sign.current()).into(), |cb| {
@@ -106,19 +111,17 @@ impl<F: Field> ExecutionGadget<F> for OpF32AddGadget<F> {
             cb.require_boolean("is sign is same, `exp` can grow by zero or one", growing_exp());
             // If `out_exp` is growing, than second mant must satisfy.
             // Second mantissa for check is already truncated.
-            let second_mant_for_check = || out_mant() * (growing_exp() + 1.expr()) - first_mant();
+            let second_mant_for_check = || (out_mant() * (growing_exp() + 1.expr()) - first_mant()) * pow2.current();
+            cb.fixed_lookup(FixedTableTag::Pow2, [
+                first_exp() - second_exp(),
+                pow2.current(),
+                0.expr(),
+            ]);
+            let dif = || (second_mant() - second_mant_for_check()) * (1.expr() - lhs_sign_bit * 2.expr());
+            let lt_gadget = cb.lt_gadget(pow2.current(), dif());
+            cb.require_zero("rest of mantissa must be shifted out of bits", lt_gadget.expr());
+            opt_lt_gadget = Some(lt_gadget);
         });
-
-/*
-        cb.fixed_lookup(
-            FixedTableTag::Pow2,
-            [
-                lhs_exp.current(),
-                odd() * is_ctz.expr(),
-                terms[i].expr() * is_ctz.expr() + 16.expr() * (1.expr() - is_ctz.expr()),
-            ],
-        );
-*/
 
         Self {
             lhs_exp_ge,
@@ -131,9 +134,8 @@ impl<F: Field> ExecutionGadget<F> for OpF32AddGadget<F> {
             lhs_limbs,
             rhs_limbs,
             out_limbs,
-            lhs_exp_shift,
-            rhs_exp_shift,
-            out_exp_shift,
+            pow2,
+            lt_gadget: opt_lt_gadget,
             _pd: Default::default(),
         }
     }
@@ -162,11 +164,12 @@ impl<F: Field> ExecutionGadget<F> for OpF32AddGadget<F> {
 
         // Here in last limb, extra bit is added, bit number 24 in mantissa that always one.
         // TODO: case with un normalized form.
-        let lhs_limbs = [lhs_raw & 0xff, (lhs_raw >> 8) & 0xff, ((lhs_raw >> 16) & 0x7f) | 0x7f];
-        let rhs_limbs = [rhs_raw & 0xff, (rhs_raw >> 8) & 0xff, ((rhs_raw >> 16) & 0x7f) | 0x7f];
-        let out_limbs = [out_raw & 0xff, (out_raw >> 8) & 0xff, ((out_raw >> 16) & 0x7f) | 0x7f];
+        let lhs_limbs = [lhs_raw & 0xff, (lhs_raw >> 8) & 0xff, ((lhs_raw >> 16) & 0x7f) | 0x80];
+        let rhs_limbs = [rhs_raw & 0xff, (rhs_raw >> 8) & 0xff, ((rhs_raw >> 16) & 0x7f) | 0x80];
+        let out_limbs = [out_raw & 0xff, (out_raw >> 8) & 0xff, ((out_raw >> 16) & 0x7f) | 0x80];
 
-        self.lhs_exp_ge.assign(region, offset, lhs_exp >= rhs_exp);
+        let lhs_exp_ge = lhs_exp >= rhs_exp;
+        self.lhs_exp_ge.assign(region, offset, lhs_exp_ge);
 
         self.lhs_sign.assign(region, offset, lhs_sign);
         self.rhs_sign.assign(region, offset, rhs_sign);
@@ -182,6 +185,38 @@ impl<F: Field> ExecutionGadget<F> for OpF32AddGadget<F> {
             self.out_limbs[i].assign(region, offset, F::from(out_limbs[i] as u64));
         }
 
+        let lhs_mant_abs = (lhs_raw & 0x7fffff) | 0x800000;
+        let rhs_mant_abs = (rhs_raw & 0x7fffff) | 0x800000;
+        let out_mant_abs = (out_raw & 0x7fffff) | 0x800000;
+
+        let lhs_mant = if lhs_sign { -(lhs_mant_abs as i64) } else { lhs_mant_abs as i64 };
+        let rhs_mant = if rhs_sign { -(rhs_mant_abs as i64) } else { rhs_mant_abs as i64 };
+        let out_mant = if out_sign { -(out_mant_abs as i64) } else { out_mant_abs as i64 };
+
+        let first_exp = if lhs_exp_ge { lhs_exp } else { rhs_exp };
+        let second_exp = if lhs_exp_ge { rhs_exp } else { lhs_exp };
+
+        let first_mant = if lhs_exp_ge { lhs_mant } else { rhs_mant };
+        let second_mant = if lhs_exp_ge { rhs_mant } else { lhs_mant };
+
+        if lhs_sign == rhs_sign {
+            let growing_exp = out_exp - first_exp;
+            assert!(growing_exp >= 0 && growing_exp <= 1);
+            let to_shift = first_exp - second_exp;
+            let pow2 = 1 << to_shift;
+            self.pow2.assign(region, offset, F::from(pow2 as u64));
+            let second_mant_for_check = (out_mant * (growing_exp as i64 + 1_i64) - first_mant) * pow2 as i64;
+            let dif = (second_mant - second_mant_for_check) * (1_i64 - lhs_sign as i64 * 2_i64);
+            println!("POW2 {}", pow2);
+            println!("DIF {}", dif);
+            self.lt_gadget.as_ref().unwrap().assign(
+                region,
+                offset,
+                F::from(pow2 as u64),
+                F::from(dif as u64),
+            );
+        }
+
         Ok(())
     }
 }
@@ -194,18 +229,30 @@ mod test {
     // using https://www.h-schmidt.net/FloatConverter/IEEE754.html.
 
     #[test]
-    fn table_f32_add_simple() {
+    fn test_f32_add_extra_bit_not_shifted_out() {
         test_ok(instruction_set! {
             I32Const(0x12800025) // 0_00100101__0000000_00000000_00100101, 8.07797129917e-28
             I32Const(0x0d00001a) // 0_00011010__0000000_00000000_00011010, 3.94431675125e-31
                                  //            ^                    ^^^^^
-                                 //            extra bit            shifted out
-
+                                 //            extra bit            shifted out (number 26)
+                                 //                                 so our `dif` is 26
+                                 //                                 this is less than 2^11, so it is really shifted out
             F32Add
             // Out   0x12801025     0_00100101__0000000_00010000_00100101, 8.08191560369e-28
             //                                             ^
             //                                             extra bit in result, shifted by 11
             //                                             exp: 37 - 26 = 11
+            Drop
+        });
+    }
+
+    #[test]
+    fn test_f32_add_doubling() {
+        test_ok(instruction_set! {
+            I32Const(0x12800025) // 0_00100101__0000000_00000000_00100101, 8.07797129917e-28
+            I32Const(0x12800025)
+            F32Add
+            // Out   0x13000025     0_00100110__0000000_00000000_00100101, 1.61559425983e-27
             Drop
         });
     }
