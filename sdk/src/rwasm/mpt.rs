d1963638f63f225e32d4bcf43948cb4a324ff1e1
Dmitry Savonin <nwtsoas@gmail.com>
2023-11-08 01:02:54 +0300

    implemented sdk for mpt and create boilerplate for zktrie

--- sdk/src/rwasm/mpt.rs
+++ sdk/src/rwasm/mpt.rs
@@ -1,33 +1,37 @@
+use crate::{MptPlatformSDK, SDK};
+
 extern "C" {
     fn _mpt_open();
-    fn _mpt_update(key_offset: i32, key_len: i32, value_offset: i32, value_len: i32);
-    fn _mpt_get(key_offset: i32, key_len: i32, output_offset: i32) -> i32;
-    fn _mpt_get_root(output_offset: i32) -> i32;
+    fn _mpt_update(key_offset: *const u8, key_len: i32, value_offset: *const u8, value_len: i32);
+    fn _mpt_get(key_offset: *const u8, key_len: i32, output_offset: *mut u8) -> i32;
+    fn _mpt_get_root(output_offset: *mut u8) -> i32;
 }
 
-#[inline(always)]
-pub fn mpt_open() {
-    unsafe { _mpt_open() }
-}
-
-#[inline(always)]
-pub fn mpt_update(key: &[u8], value: &[u8]) {
-    unsafe {
-        _mpt_update(
-            key.as_ptr() as i32,
-            key.len() as i32,
-            value.as_ptr() as i32,
-            value.len() as i32,
-        )
+impl MptPlatformSDK for SDK {
+    #[inline(always)]
+    fn mpt_open() {
+        unsafe { _mpt_open() }
     }
-}
 
-#[inline(always)]
-pub fn mpt_get(key_offset: i32, key_len: i32, output_offset: i32) -> i32 {
-    unsafe { _mpt_get(key_offset, key_len, output_offset) }
-}
+    #[inline(always)]
+    fn mpt_update(key: &[u8], value: &[u8]) {
+        unsafe {
+            _mpt_update(
+                key.as_ptr(),
+                key.len() as i32,
+                value.as_ptr(),
+                value.len() as i32,
+            )
+        }
+    }
 
-#[inline(always)]
-pub fn mpt_get_root(output_offset: i32) -> i32 {
-    unsafe { _mpt_get_root(output_offset) }
+    #[inline(always)]
+    fn mpt_get(key: &[u8], output: &mut [u8]) -> i32 {
+        unsafe { _mpt_get(key.as_ptr(), key.len() as i32, output.as_mut_ptr()) }
+    }
+
+    #[inline(always)]
+    fn mpt_root(output: &mut [u8]) -> i32 {
+        unsafe { _mpt_get_root(output.as_mut_ptr()) }
+    }
 }
--- sdk/src/rwasm/zktrie.rs
+++ sdk/src/rwasm/zktrie.rs
@@ -1,3 +1,5 @@
+use crate::{ZktriePlatformSDK, SDK};
+
 extern "C" {
     fn _zktrie_open();
     fn _zktrie_update_nonce(key_offset: i32, key_len: i32, value_offset: i32, value_len: i32);
@@ -19,72 +21,74 @@ extern "C" {
     fn _zktrie_get_store(key_offset: i32, key_len: i32, output_offset: i32);
 }
 
-#[inline(always)]
-pub fn zktrie_open() {
-    unsafe { _zktrie_open() }
-}
+impl ZktriePlatformSDK for SDK {
+    #[inline(always)]
+    fn zktrie_open() {
+        unsafe { _zktrie_open() }
+    }
 
-#[inline(always)]
-pub fn zktrie_update_nonce(key_offset: i32, key_len: i32, value_offset: i32, value_len: i32) {
-    unsafe { _zktrie_update_nonce(key_offset, key_len, value_offset, value_len) }
-}
+    #[inline(always)]
+    fn zktrie_update_nonce(key_offset: i32, key_len: i32, value_offset: i32, value_len: i32) {
+        unsafe { _zktrie_update_nonce(key_offset, key_len, value_offset, value_len) }
+    }
 
-#[inline(always)]
-pub fn zktrie_update_balance(key_offset: i32, key_len: i32, value_offset: i32, value_len: i32) {
-    unsafe { _zktrie_update_balance(key_offset, key_len, value_offset, value_len) }
-}
+    #[inline(always)]
+    fn zktrie_update_balance(key_offset: i32, key_len: i32, value_offset: i32, value_len: i32) {
+        unsafe { _zktrie_update_balance(key_offset, key_len, value_offset, value_len) }
+    }
 
-#[inline(always)]
-pub fn zktrie_update_storage_root(
-    key_offset: i32,
-    key_len: i32,
-    value_offset: i32,
-    value_len: i32,
-) {
-    unsafe { _zktrie_update_storage_root(key_offset, key_len, value_offset, value_len) }
-}
+    #[inline(always)]
+    fn zktrie_update_storage_root(
+        key_offset: i32,
+        key_len: i32,
+        value_offset: i32,
+        value_len: i32,
+    ) {
+        unsafe { _zktrie_update_storage_root(key_offset, key_len, value_offset, value_len) }
+    }
 
-#[inline(always)]
-pub fn zktrie_update_code_hash(key_offset: i32, key_len: i32, value_offset: i32, value_len: i32) {
-    unsafe { _zktrie_update_code_hash(key_offset, key_len, value_offset, value_len) }
-}
+    #[inline(always)]
+    fn zktrie_update_code_hash(key_offset: i32, key_len: i32, value_offset: i32, value_len: i32) {
+        unsafe { _zktrie_update_code_hash(key_offset, key_len, value_offset, value_len) }
+    }
 
-#[inline(always)]
-pub fn zktrie_update_code_size(key_offset: i32, key_len: i32, value_offset: i32, value_len: i32) {
-    unsafe { _zktrie_update_code_size(key_offset, key_len, value_offset, value_len) }
-}
+    #[inline(always)]
+    fn zktrie_update_code_size(key_offset: i32, key_len: i32, value_offset: i32, value_len: i32) {
+        unsafe { _zktrie_update_code_size(key_offset, key_len, value_offset, value_len) }
+    }
 
-#[inline(always)]
-pub fn zktrie_get_nonce(key_offset: i32, key_len: i32, output_offset: i32) {
-    unsafe { _zktrie_get_nonce(key_offset, key_len, output_offset) }
-}
+    #[inline(always)]
+    fn zktrie_get_nonce(key_offset: i32, key_len: i32, output_offset: i32) {
+        unsafe { _zktrie_get_nonce(key_offset, key_len, output_offset) }
+    }
 
-#[inline(always)]
-pub fn zktrie_get_balance(key_offset: i32, key_len: i32, output_offset: i32) {
-    unsafe { _zktrie_get_balance(key_offset, key_len, output_offset) }
-}
+    #[inline(always)]
+    fn zktrie_get_balance(key_offset: i32, key_len: i32, output_offset: i32) {
+        unsafe { _zktrie_get_balance(key_offset, key_len, output_offset) }
+    }
 
-#[inline(always)]
-pub fn zktrie_get_storage_root(key_offset: i32, key_len: i32, output_offset: i32) {
-    unsafe { _zktrie_get_storage_root(key_offset, key_len, output_offset) }
-}
+    #[inline(always)]
+    fn zktrie_get_storage_root(key_offset: i32, key_len: i32, output_offset: i32) {
+        unsafe { _zktrie_get_storage_root(key_offset, key_len, output_offset) }
+    }
 
-#[inline(always)]
-pub fn zktrie_get_code_hash(key_offset: i32, key_len: i32, output_offset: i32) {
-    unsafe { _zktrie_get_code_hash(key_offset, key_len, output_offset) }
-}
+    #[inline(always)]
+    fn zktrie_get_code_hash(key_offset: i32, key_len: i32, output_offset: i32) {
+        unsafe { _zktrie_get_code_hash(key_offset, key_len, output_offset) }
+    }
 
-#[inline(always)]
-pub fn zktrie_get_code_size(key_offset: i32, key_len: i32, output_offset: i32) {
-    unsafe { _zktrie_get_code_size(key_offset, key_len, output_offset) }
-}
+    #[inline(always)]
+    fn zktrie_get_code_size(key_offset: i32, key_len: i32, output_offset: i32) {
+        unsafe { _zktrie_get_code_size(key_offset, key_len, output_offset) }
+    }
 
-#[inline(always)]
-pub fn zktrie_update_store(key_offset: i32, key_len: i32, value_offset: i32, value_len: i32) {
-    unsafe { _zktrie_update_store(key_offset, key_len, value_offset, value_len) }
-}
+    #[inline(always)]
+    fn zktrie_update_store(key_offset: i32, key_len: i32, value_offset: i32, value_len: i32) {
+        unsafe { _zktrie_update_store(key_offset, key_len, value_offset, value_len) }
+    }
 
-#[inline(always)]
-pub fn zktrie_get_store(key_offset: i32, key_len: i32, output_offset: i32) {
-    unsafe { _zktrie_get_store(key_offset, key_len, output_offset) }
+    #[inline(always)]
+    fn zktrie_get_store(key_offset: i32, key_len: i32, output_offset: i32) {
+        unsafe { _zktrie_get_store(key_offset, key_len, output_offset) }
+    }
 }
