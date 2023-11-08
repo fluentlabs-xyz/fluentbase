782b83e77b7af5b34a95ff1cc4fb53a18e41092c
Dmitry Savonin <nwtsoas@gmail.com>
2023-11-08 18:11:27 +0300

    added router for rwasm compile function

--- sdk/src/runtime/rwasm.rs
+++ sdk/src/runtime/rwasm.rs
@@ -1,11 +1,18 @@
 use crate::{RwasmPlatformSDK, SDK};
-use fluentbase_runtime::{ExitCode, Runtime, RuntimeContext};
-use fluentbase_rwasm::rwasm::Compiler;
+use fluentbase_runtime::{ExitCode, Runtime, RuntimeContext, SysFuncIdx};
+use fluentbase_rwasm::engine::bytecode::Instruction;
+use fluentbase_rwasm::rwasm::{Compiler, FuncOrExport};
 
 impl RwasmPlatformSDK for SDK {
     fn rwasm_compile(input: &[u8], output: &mut [u8]) -> i32 {
         let import_linker = Runtime::new_linker();
         let mut compiler = Compiler::new_with_linker(input.as_ref(), Some(&import_linker)).unwrap();
+        compiler
+            .translate(Some(FuncOrExport::StateRouter(
+                vec![FuncOrExport::Export("deploy"), FuncOrExport::Export("main")],
+                Instruction::Call(SysFuncIdx::SYS_STATE.into()),
+            )))
+            .unwrap();
         let rwasm_bytecode = compiler.finalize().unwrap();
         if rwasm_bytecode.len() <= output.len() {
             output.copy_from_slice(rwasm_bytecode.as_slice());
