3c30104bdb61641735dd38a025e7333922711836
Dmitry Savonin <nwtsoas@gmail.com>
2023-11-09 16:44:04 +0300

    moved examples folder, added evm folder for rwasm, tiny sdk fixes

--- examples/src/keccak256.rs
+++ examples/src/keccak256.rs
@@ -2,7 +2,7 @@ use fluentbase_sdk::{CryptoPlatformSDK, SysPlatformSDK, SDK};
 
 pub fn main() {
     let mut input = [0u8; 11]; // "hello world"
-    SDK::sys_read_slice(&mut input, 0);
+    SDK::sys_read(&mut input, 0);
     let mut output = [0u8; 32];
     SDK::crypto_keccak256(&input, &mut output);
 }
