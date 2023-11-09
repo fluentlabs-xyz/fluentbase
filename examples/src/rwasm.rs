a446ff9ec3534452da5bb8fba41406017da4c49a
Dmitry Savonin <nwtsoas@gmail.com>
2023-11-09 19:51:11 +0300

    fixed sdk and runtime

--- examples/src/rwasm.rs
+++ examples/src/rwasm.rs
@@ -3,8 +3,8 @@ use fluentbase_sdk::{SysPlatformSDK, SDK};
 
 pub fn main() {
     let mut wasm_bytecode: [u8; 1024] = [0; 1024];
-    SDK::sys_read_slice(&mut wasm_bytecode, 0);
+    SDK::sys_read(&mut wasm_bytecode, 0);
     let mut compiler = Compiler::new(&wasm_bytecode).unwrap();
     let rwasm_bytecode = compiler.finalize().unwrap();
-    SDK::sys_write_slice(rwasm_bytecode.as_slice());
+    SDK::sys_write(rwasm_bytecode.as_slice());
 }
