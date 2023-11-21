afeac4279b46ed374882bd407297e1716cbb8a5f
Dmitry Savonin <nwtsoas@gmail.com>
2023-11-21 17:54:33 +0300

    removed circuit crate

--- fluent/src/eth_types/mod.rs
+++
@@ -1,8 +0,0 @@
-pub(crate) mod block;
-pub(crate) mod bytes;
-pub(crate) mod gas_utils;
-pub(crate) mod header;
-pub(crate) mod proof;
-pub(crate) mod receipt;
-pub(crate) mod transaction;
-pub(crate) mod tx_types;
