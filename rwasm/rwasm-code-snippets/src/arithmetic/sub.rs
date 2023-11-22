e80c6bd5e3c2323a6671d9b64bb55bd45c0ffab2
Stas Stepanov <stanislav@ankr.com>
2023-11-22 12:34:33 +0600

    sub opcode snippet

--- rwasm/rwasm-code-snippets/src/arithmetic/sub.rs
+++ rwasm/rwasm-code-snippets/src/arithmetic/sub.rs
@@ -10,16 +10,48 @@ fn arithmetic_sub(
     b3: u64,
 ) -> (u64, u64, u64, u64) {
     let a0_sign = a0 & 0x8000000000000000;
-    let b0_sign = b0 & 0x8000000000000000;
 
-    if a0_sign > b0_sign {
-        // -a - b = -(a+b)
-    }
-    if b0_sign > a0_sign {
-        // a - -b = a+b
-    }
-    // a - b
-    // -a + b = b-a
+    let mut borrow = 0;
+    let s0;
+    let s1;
+    let s2;
+    let s3;
 
-    (0, 0, 0, 0)
+    if a3 >= b3 {
+        s3 = a3 - b3;
+    } else {
+        s3 = 0xffffffffffffffff - b3 + a3 + 1;
+        borrow = 1;
+    }
+
+    if a2 >= b2 + borrow {
+        s2 = a2 - b2 - borrow;
+        borrow = 0;
+    } else {
+        s2 = 0xffffffffffffffff - b2 + a2 + 1;
+        borrow = 1;
+    }
+
+    if a1 >= b1 + borrow {
+        s1 = a1 - b1 - borrow;
+        borrow = 0;
+    } else {
+        s1 = 0xffffffffffffffff - b1 + a1 + 1;
+        borrow = 1;
+    }
+
+    if a0 >= b0 + borrow {
+        s0 = a0 - b0 - borrow;
+        // borrowed = 0;
+    } else {
+        if a0_sign > 0 {
+            s0 = 0xffffffffffffffff - b0 + a0 + 1;
+            // borrowed = 1;
+        } else {
+            // TODO process overflow
+            s0 = 0x8000000000000000;
+        }
+    }
+
+    (s0, s1, s2, s3)
 }
