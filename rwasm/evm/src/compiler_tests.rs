a4c29008dc388594553098037d54e5f7c9fe5b5b
Stas Stepanov <stanislav@ankr.com>
2023-11-26 22:32:47 +0600

    expand GT test with test cases

--- rwasm/evm/src/compiler_tests.rs
+++ rwasm/evm/src/compiler_tests.rs
@@ -17,7 +17,7 @@ mod evm_to_rwasm_tests {
     };
     use log::debug;
 
-    fn test(evm_bytecode_bytes: &Vec<u8>) {
+    fn run_test(evm_bytecode_bytes: &Vec<u8>) -> Vec<u8> {
         let evm_binary = Bytes::from(evm_bytecode_bytes.clone());
 
         let mut compiler = EvmCompiler::new(evm_binary.as_ref(), false);
@@ -49,19 +49,19 @@ mod evm_to_rwasm_tests {
             instruction_set.trace()
         );
 
-        let mut global_memory = vec![0u8; 1024 * 1024];
+        let mut global_memory = vec![0u8; 4086];
         let mut global_memory_len: usize = 0;
         let result = Runtime::run(&rwasm_binary, &Vec::new(), 0);
         assert!(result.is_ok());
         let execution_result = result.unwrap();
-        debug!("mem changes:");
+        // debug!("mem changes:");
         for log in execution_result.tracer().logs.iter() {
-            if log.memory_changes.len() > 0 {
-                debug!(
-                    "log opcode {} memory_changes {:?}",
-                    log.opcode, &log.memory_changes
-                );
-            }
+            // if log.memory_changes.len() > 0 {
+            //     debug!(
+            //         "log opcode {} memory_changes {:?}",
+            //         log.opcode, &log.memory_changes
+            //     );
+            // }
             for change in &log.memory_changes {
                 let new_len = (change.offset + change.len) as usize;
                 global_memory[change.offset as usize..new_len].copy_from_slice(&change.data);
@@ -70,13 +70,20 @@ mod evm_to_rwasm_tests {
                 }
             }
         }
-        debug!("global_memory {:?}", &global_memory[..global_memory_len]);
+        let global_memory = global_memory.to_vec();
+        debug!(
+            "global_memory (len {}) {:?}",
+            global_memory_len,
+            &global_memory[..global_memory_len]
+        );
         debug!(
             "\nexecution_result.tracer() (exit_code {}): \n{:#?}\n",
             execution_result.data().exit_code(),
             execution_result.tracer()
         );
         assert_eq!(execution_result.data().exit_code(), 0);
+
+        global_memory
     }
 
     #[test]
@@ -85,7 +92,7 @@ mod evm_to_rwasm_tests {
             PUSH0, PUSH1, 0x80, PUSH1, 0x40, PUSH8, 0, 0, 0, 0, 0, 0, 1, 246,
         ];
 
-        test(&evm_bytecode_bytes);
+        run_test(&evm_bytecode_bytes);
     }
 
     #[test]
@@ -99,7 +106,7 @@ mod evm_to_rwasm_tests {
             PUSH1, offset, PUSH1, a0, PUSH1, b0, EQ,
         ];
 
-        test(&evm_bytecode_bytes);
+        run_test(&evm_bytecode_bytes);
     }
 
     #[test]
@@ -113,7 +120,7 @@ mod evm_to_rwasm_tests {
             PUSH1, offset, PUSH1, a0, PUSH1, b0, SHL,
         ];
 
-        test(&evm_bytecode_bytes);
+        run_test(&evm_bytecode_bytes);
     }
 
     #[test]
@@ -131,30 +138,36 @@ mod evm_to_rwasm_tests {
             b0_0, LT,
         ];
 
-        test(&evm_bytecode_bytes);
+        run_test(&evm_bytecode_bytes);
     }
 
     #[test]
     fn gt_opcode() {
         let offset = 0;
-        let a0_0 = 1;
-        let a1_0 = 2;
-
-        let b0_0 = 1;
-        let b1_0 = 1;
-        // if a > b
-        let evm_bytecode_bytes: Vec<u8> = vec![
-            // op: `mem_offset` a=1 b=0
-            // TODO need evm preprocessing to automatically insert offset arg (PUSH1 0)
-            PUSH1, offset, PUSH9, a1_0, 0, 0, 0, 0, 0, 0, 0, a0_0, PUSH9, b1_0, 0, 0, 0, 0, 0, 0, 0,
-            b0_0, GT,
+        // BE repr of A and B params and expected RESULT
+        let test_cases = [
+            ((0, 1), (0, 1), 0u8),
+            ((0, 2), (0, 1), 1u8),
+            ((2, 1), (1, 1), 1u8),
+            ((1, 1), (1, 1), 0),
         ];
-        // let evm_bytecode_bytes: Vec<u8> = vec![
-        //     // op: `mem_offset` a=1 b=0
-        //     // TODO need evm preprocessing to automatically insert offset arg (PUSH1 0)
-        //     PUSH1, offset, PUSH1, a0_0, PUSH1, b0_0, GT,
-        // ];
+        for case in &test_cases {
+            let a1_0 = case.0 .0;
+            let a0_0 = case.0 .1;
+            let b1_0 = case.1 .0;
+            let b0_0 = case.1 .1;
+            let res_expected = case.2;
+            // if a > b
+            let evm_bytecode_bytes: Vec<u8> = vec![
+                // op: `mem_offset` a=1 b=0
+                // TODO need evm preprocessing to automatically insert offset arg (PUSH1 0)
+                PUSH1, offset, PUSH9, a1_0, 0, 0, 0, 0, 0, 0, 0, a0_0, PUSH9, b1_0, 0, 0, 0, 0, 0,
+                0, 0, b0_0, GT,
+            ];
 
-        test(&evm_bytecode_bytes);
+            let global_memory = run_test(&evm_bytecode_bytes);
+            let res = global_memory[32 - 8];
+            assert_eq!(res, res_expected);
+        }
     }
 }
