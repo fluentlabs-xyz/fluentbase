17a95b1ca6acbbd1855872fbdacadcd50800663b
Stas Stepanov <stanislav@ankr.com>
2023-12-07 17:47:23 +0600

    make 'signextend' rust impl more generic

--- rwasm/rwasm-code-snippets/src/arithmetic/signextend.rs
+++ rwasm/rwasm-code-snippets/src/arithmetic/signextend.rs
@@ -3,6 +3,7 @@ use crate::consts::{
     BYTE_SIGN_BIT_MASK,
     U64_ALL_BITS_ARE_0,
     U64_ALL_BITS_ARE_1,
+    U64_BYTES_COUNT,
     U64_HALF_BITS_COUNT,
     U64_LSBYTE_MASK,
 };
@@ -18,75 +19,27 @@ pub fn arithmetic_signextend(
     size2: u64,
     size3: u64,
 ) -> (u64, u64, u64, u64) {
-    let mut s0: u64 = value0;
-    let mut s1: u64 = value1;
-    let mut s2: u64 = value2;
-    let mut s3: u64 = value3;
+    let mut res = [value0, value1, value2, value3];
 
     if size0 < U64_HALF_BITS_COUNT && size1 == 0 && size2 == 0 && size3 == 0 {
         let mut byte_value: u8 = 0;
-        if size0 < 8 {
-            let shift = size0 * BITS_IN_BYTE;
-            byte_value = ((value0 >> shift) & U64_LSBYTE_MASK) as u8;
-            if byte_value & BYTE_SIGN_BIT_MASK as u8 > 0 {
-                let shift = shift + BITS_IN_BYTE;
-                // fill with FF
-                s0 = (U64_ALL_BITS_ARE_1 << shift) | value0;
-                s1 = U64_ALL_BITS_ARE_1;
-                s2 = U64_ALL_BITS_ARE_1;
-                s3 = U64_ALL_BITS_ARE_1;
-            } else {
-                let shift = (7 - size0) * BITS_IN_BYTE;
-                // fill with 00
-                s0 = (U64_ALL_BITS_ARE_1 >> shift) & value0;
-                s1 = U64_ALL_BITS_ARE_0;
-                s2 = U64_ALL_BITS_ARE_0;
-                s3 = U64_ALL_BITS_ARE_0;
-            }
-        } else if size0 < 16 {
-            let shift = (size0 - 8) * BITS_IN_BYTE;
-            byte_value = ((value1 >> shift) & U64_LSBYTE_MASK) as u8;
-            if byte_value & BYTE_SIGN_BIT_MASK as u8 > 0 {
-                let shift = shift + BITS_IN_BYTE;
-                // fill with FF
-                s1 = (U64_ALL_BITS_ARE_1 << shift) | value1;
-                s2 = U64_ALL_BITS_ARE_1;
-                s3 = U64_ALL_BITS_ARE_1;
-            } else {
-                let shift = (7 - size0) * BITS_IN_BYTE;
-                // fill with 00
-                s1 = (U64_ALL_BITS_ARE_1 >> shift) & value1;
-                s2 = U64_ALL_BITS_ARE_0;
-                s3 = U64_ALL_BITS_ARE_0;
-            }
-        } else if size0 < 24 {
-            let shift = (size0 - 16) * BITS_IN_BYTE;
-            byte_value = ((value2 >> shift) & U64_LSBYTE_MASK) as u8;
-            if byte_value & BYTE_SIGN_BIT_MASK as u8 > 0 {
-                let shift = shift + BITS_IN_BYTE;
-                // fill with FF
-                s2 = (U64_ALL_BITS_ARE_1 << shift) | value2;
-                s3 = U64_ALL_BITS_ARE_1;
-            } else {
-                let shift = (7 - size0) * BITS_IN_BYTE;
-                // fill with 00
-                s2 = (U64_ALL_BITS_ARE_1 >> shift) & value2;
-                s3 = U64_ALL_BITS_ARE_0;
-            }
+        let res_idx = size0 as usize / 8;
+        let filler: u64;
+        let shift = (size0 - res_idx as u64 * U64_BYTES_COUNT) * BITS_IN_BYTE;
+        byte_value = ((res[res_idx] >> shift) & U64_LSBYTE_MASK) as u8;
+        if byte_value >= BYTE_SIGN_BIT_MASK as u8 {
+            let shift = shift + BITS_IN_BYTE;
+            res[res_idx] = (U64_ALL_BITS_ARE_1 << shift) | res[res_idx];
+            filler = U64_ALL_BITS_ARE_1;
         } else {
-            let shift = (size0 - 24) * BITS_IN_BYTE;
-            byte_value = ((value3 >> shift) & U64_LSBYTE_MASK) as u8;
-            if byte_value & BYTE_SIGN_BIT_MASK as u8 > 0 {
-                let shift = shift + BITS_IN_BYTE;
-                // fill with FF
-                s3 = (U64_ALL_BITS_ARE_1 << shift) | value3;
-            } else {
-                let shift = (7 - size0) * BITS_IN_BYTE;
-                // fill with 00
-                s3 = (U64_ALL_BITS_ARE_1 >> shift) & value3;
-            }
+            let shift = (7 - size0) * BITS_IN_BYTE;
+            res[res_idx] = (U64_ALL_BITS_ARE_1 >> shift) & res[res_idx];
+            filler = U64_ALL_BITS_ARE_0;
+        }
+        for i in res_idx + 1..4 {
+            res[i] = filler
         }
     }
 
-    (s0, s1, s2, s3)
+    (res[0], res[1], res[2], res[3])
 }
