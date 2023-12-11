1967f7df3f14abdcba0a48cb2db6e26af5fd06b6
Stas Stepanov <stanislav@ankr.com>
2023-12-11 17:14:19 +0600

    addmod

--- rwasm/rwasm-code-snippets/src/arithmetic.rs
+++ rwasm/rwasm-code-snippets/src/arithmetic.rs
@@ -1,14 +1,16 @@
-#[cfg(feature = "arithmetic_mod")]
-pub mod a_mod;
-#[cfg(feature = "arithmetic_smod")]
-pub mod a_smod;
 #[cfg(feature = "arithmetic_add")]
 pub mod add;
+#[cfg(feature = "arithmetic_addmod")]
+pub mod addmod;
 #[cfg(feature = "arithmetic_div")]
 pub mod div;
+#[cfg(feature = "arithmetic_mod")]
+pub mod mod_impl;
 #[cfg(feature = "arithmetic_mul")]
 pub mod mul;
 #[cfg(feature = "arithmetic_sdiv")]
 pub mod sdiv;
 #[cfg(feature = "arithmetic_signextend")]
 mod signextend;
+#[cfg(feature = "arithmetic_smod")]
+pub mod smod_impl;
--- rwasm/rwasm-code-snippets/src/arithmetic/add.rs
+++ rwasm/rwasm-code-snippets/src/arithmetic/add.rs
@@ -1,4 +1,4 @@
-use crate::consts::{U64_HALF_BITS_COUNT, U64_LOW_PART_MASK};
+use crate::common::add;
 
 #[no_mangle]
 pub fn arithmetic_add(
@@ -11,57 +11,5 @@ pub fn arithmetic_add(
     a2: u64,
     a3: u64,
 ) -> (u64, u64, u64, u64) {
-    let mut a_part: u64 = 0;
-    let mut b_part: u64 = 0;
-    let mut part_sum: u64 = 0;
-    let mut carry: u64 = 0;
-    let mut s0: u64 = 0;
-    let mut s1: u64 = 0;
-    let mut s2: u64 = 0;
-    let mut s3: u64 = 0;
-
-    a_part = a0 & U64_LOW_PART_MASK;
-    b_part = b0 & U64_LOW_PART_MASK;
-    part_sum = a_part + b_part;
-    s0 = part_sum & U64_LOW_PART_MASK;
-    carry = part_sum >> U64_HALF_BITS_COUNT;
-    a_part = a0 >> U64_HALF_BITS_COUNT;
-    b_part = b0 >> U64_HALF_BITS_COUNT;
-    part_sum = a_part + b_part + carry;
-    s0 = s0 + ((part_sum & U64_LOW_PART_MASK) << U64_HALF_BITS_COUNT);
-    carry = part_sum >> U64_HALF_BITS_COUNT;
-
-    a_part = a1 & U64_LOW_PART_MASK;
-    b_part = b1 & U64_LOW_PART_MASK;
-    part_sum = a_part + b_part + carry;
-    s1 = part_sum & U64_LOW_PART_MASK;
-    carry = part_sum >> U64_HALF_BITS_COUNT;
-    a_part = a1 >> U64_HALF_BITS_COUNT;
-    b_part = b1 >> U64_HALF_BITS_COUNT;
-    part_sum = a_part + b_part + carry;
-    s1 = s1 + ((part_sum & U64_LOW_PART_MASK) << U64_HALF_BITS_COUNT);
-    carry = part_sum >> U64_HALF_BITS_COUNT;
-
-    a_part = a2 & U64_LOW_PART_MASK;
-    b_part = b2 & U64_LOW_PART_MASK;
-    part_sum = a_part + b_part + carry;
-    s2 = part_sum & U64_LOW_PART_MASK;
-    carry = part_sum >> U64_HALF_BITS_COUNT;
-    a_part = a2 >> U64_HALF_BITS_COUNT;
-    b_part = b2 >> U64_HALF_BITS_COUNT;
-    part_sum = a_part + b_part + carry;
-    s2 = s2 + ((part_sum & U64_LOW_PART_MASK) << U64_HALF_BITS_COUNT);
-    carry = part_sum >> U64_HALF_BITS_COUNT;
-
-    a_part = a3 & U64_LOW_PART_MASK;
-    b_part = b3 & U64_LOW_PART_MASK;
-    part_sum = a_part + b_part + carry;
-    s3 = part_sum & U64_LOW_PART_MASK;
-    carry = part_sum >> U64_HALF_BITS_COUNT;
-    a_part = a3 >> U64_HALF_BITS_COUNT;
-    b_part = b3 >> U64_HALF_BITS_COUNT;
-    part_sum = a_part + b_part + carry;
-    s3 = s3 + ((part_sum & U64_LOW_PART_MASK) << U64_HALF_BITS_COUNT);
-
-    (s0, s1, s2, s3)
+    add(a0, a1, a2, a3, b0, b1, b2, b3)
 }
