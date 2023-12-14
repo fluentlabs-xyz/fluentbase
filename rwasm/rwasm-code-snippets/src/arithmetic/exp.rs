dd80eb675f35af145f0e4d5bb97a8268d03e0ecd
Stas Stepanov <stanislav@ankr.com>
2023-12-14 21:17:12 +0600

    uncomment exp tests

--- rwasm/rwasm-code-snippets/src/arithmetic/exp.rs
+++ rwasm/rwasm-code-snippets/src/arithmetic/exp.rs
@@ -1,11 +1,4 @@
-use crate::common::mul_test;
-
-// #[no_mangle]
-// fn some_fn(a: u64, b: u64) -> u64 {
-//     // let mut r = (0, 0, 0, 0);
-//     // r.0 = a.0 | b.0;
-//     a | b
-// }
+use crate::common::exp;
 
 #[no_mangle]
 pub fn arithmetic_exp(
@@ -18,11 +11,5 @@ pub fn arithmetic_exp(
     v2: u64,
     v3: u64,
 ) -> (u64, u64, u64, u64) {
-    let mut r = (1, 1, 1, 1);
-    // r.0 = add_test(r.0, exp0);
-    // r.0 = add_test(r.0, v0);
-    r = mul_test(r, (exp0, exp1, exp2, exp3));
-    r = mul_test(r, (v0, v1, v2, v3));
-    // exp(v0, v1, v2, v3, exp0, exp1, exp2, exp3)
-    (r.0, r.1, r.2, r.3)
+    exp(v0, v1, v2, v3, exp0, exp1, exp2, exp3)
 }
