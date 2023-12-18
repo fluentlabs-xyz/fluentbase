d03b2ac6aa882e9e27034288e230c0833baa41bc
Dmitry Savonin <nwtsoas@gmail.com>
2023-12-18 17:12:22 +0300

    added functions for exporting stack

--- examples/deploy-contract.js
+++ examples/deploy-contract.js
@@ -1,4 +1,4 @@
-const Web3 = require('web3');
+const {Web3} = require('web3');
 const fs = require('fs');
 
 const DEPLOYER_PRIVATE_KEY = 'ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80';
