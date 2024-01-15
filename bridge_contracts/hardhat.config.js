d37366d1ec9003e5c8fbffbfb88182583de6a8a2
Bogdan Shinkarenko <shinkarenko@gmail.com>
2024-01-16 00:41:58 +1000

    added e2e tests

--- bridge_contracts/hardhat.config.js
+++ bridge_contracts/hardhat.config.js
@@ -4,4 +4,11 @@ require("@nomiclabs/hardhat-solhint");
 
 module.exports = {
   solidity: "0.8.20",
+  networks: {
+    L1: {
+      url: "http://localhost:8545",
+    },
+    L2: {
+      url: "http://localhost:8546",
+    },  },
 };
