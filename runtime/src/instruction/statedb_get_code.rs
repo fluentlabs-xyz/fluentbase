3219d3858418c6f802d1aac66131605889251d87
Dmitry Savonin <nwtsoas@gmail.com>
2024-01-26 02:41:01 +0300

    fixed compiling runtime
    Signed-off-by: Dmitry Savonin <nwtsoas@gmail.com>

--- runtime/src/instruction/statedb_get_code.rs
+++ runtime/src/instruction/statedb_get_code.rs
@@ -12,20 +12,20 @@ impl StateDbGetCode {
         output_len: u32,
     ) -> Result<(), Trap> {
         let address = caller.read_memory(key20_offset, 20).to_vec();
-        let code =
-            Self::fn_impl(caller.data(), &address, output_len).map_err(|err| err.into_trap())?;
+        let code = Self::fn_impl(caller.data_mut(), &address, output_len)
+            .map_err(|err| err.into_trap())?;
         caller.write_memory(output_offset, &code);
         Ok(())
     }
 
     pub fn fn_impl<T>(
-        context: &RuntimeContext<T>,
+        context: &mut RuntimeContext<T>,
         key: &[u8],
         output_len: u32,
     ) -> Result<Vec<u8>, ExitCode> {
         let account_db = context.account_db.clone().unwrap();
         let account = account_db
-            .borrow()
+            .borrow_mut()
             .get_account(&Address::from_slice(key))
             .unwrap_or_default();
         let code = account.code.unwrap_or_default();
