b2ad60e998c0357565b708413537339a46423281
Dmitry Savonin <nwtsoas@gmail.com>
2024-01-25 17:10:53 +0300

    added more zktrie methods boilerplates, added get/set code methods into storage
    Signed-off-by: Dmitry Savonin <nwtsoas@gmail.com>

--- runtime/src/storage/zktrie.rs
+++ runtime/src/storage/zktrie.rs
@@ -1,4 +1,4 @@
-use crate::storage::{KeyValueDb, TrieDb};
+use crate::storage::{KeyValueDb, StateDb};
 use fluentbase_types::ExitCode;
 use fluentbase_zktrie::{
     Byte32,
@@ -18,17 +18,23 @@ struct NodeDb<'a, DB>(&'a mut DB);
 
 const STORAGE_PREFIX_NODE: u8 = 0x01;
 const STORAGE_PREFIX_PREIMAGE: u8 = 0x02;
+const STORAGE_PREFIX_CODE: u8 = 0x02;
+
+macro_rules! storage_key {
+    ($prefix:ident, $key:expr) => {{
+        assert_eq!($key.len(), 32);
+        let mut storage_key = [0u8; 33];
+        storage_key[0] = $prefix;
+        storage_key[1..].copy_from_slice($key);
+        storage_key
+    }};
+}
 
 impl<'a, DB: KeyValueDb> Database for NodeDb<'a, DB> {
     type Node = Node<PoseidonHash>;
 
     fn get_node(&self, key: &Hash) -> Result<Option<Arc<Self::Node>>, Error> {
-        let storage_key = {
-            let mut storage_key = [0u8; 33];
-            storage_key[0] = STORAGE_PREFIX_NODE;
-            storage_key[1..].copy_from_slice(key.raw_bytes());
-            storage_key
-        };
+        let storage_key = storage_key!(STORAGE_PREFIX_NODE, key.raw_bytes());
         match self.0.get(&storage_key[..]) {
             Some(value) => Ok(Some(Arc::new(Node::from_bytes(value.as_slice())?))),
             None => Ok(None),
@@ -36,12 +42,7 @@ impl<'a, DB: KeyValueDb> Database for NodeDb<'a, DB> {
     }
 
     fn update_node(&mut self, node: Self::Node) -> Result<Arc<Self::Node>, Error> {
-        let storage_key = {
-            let mut storage_key = [0u8; 33];
-            storage_key[0] = STORAGE_PREFIX_NODE;
-            storage_key[1..].copy_from_slice(node.hash().raw_bytes());
-            storage_key
-        };
+        let storage_key = storage_key!(STORAGE_PREFIX_NODE, node.hash().raw_bytes());
         self.0.put(&storage_key[..], &node.canonical_value());
         Ok(Arc::new(node))
     }
@@ -49,34 +50,36 @@ impl<'a, DB: KeyValueDb> Database for NodeDb<'a, DB> {
 
 impl<'a, DB: KeyValueDb> PreimageDatabase for NodeDb<'a, DB> {
     fn update_preimage(&mut self, preimage: &[u8], hash_field: &Fr) {
-        let storage_key = {
-            let mut storage_key = [0u8; 33];
-            storage_key[0] = STORAGE_PREFIX_PREIMAGE;
-            storage_key[1..].copy_from_slice(&hash_field.to_bytes());
-            storage_key
-        };
+        let storage_key = storage_key!(STORAGE_PREFIX_PREIMAGE, &hash_field.to_bytes());
         self.0.put(&storage_key[..], &preimage.to_vec());
     }
 
     fn preimage(&self, key: &Fr) -> Vec<u8> {
-        let storage_key = {
-            let mut storage_key = [0u8; 33];
-            storage_key[0] = STORAGE_PREFIX_PREIMAGE;
-            storage_key[1..].copy_from_slice(&key.to_bytes());
-            storage_key
-        };
+        let storage_key = storage_key!(STORAGE_PREFIX_PREIMAGE, &key.to_bytes());
         self.0.get(&storage_key[..]).unwrap_or_default()
     }
 }
 
-pub struct ZkTriePersistentStorage<'a, DB> {
+impl<'a, DB: KeyValueDb> NodeDb<'a, DB> {
+    pub(crate) fn get_code(&self, key: &[u8]) -> Option<Vec<u8>> {
+        let storage_key = storage_key!(STORAGE_PREFIX_CODE, key);
+        self.0.get(&storage_key)
+    }
+
+    pub(crate) fn set_code(&mut self, key: &[u8], value: &Vec<u8>) {
+        let storage_key = storage_key!(STORAGE_PREFIX_CODE, key);
+        self.0.put(&storage_key, value);
+    }
+}
+
+pub struct ZkTrieStateDb<'a, DB> {
     storage: NodeDb<'a, DB>,
     trie: Option<ZkTrie<PoseidonHash>>,
 }
 
 const MAX_LEVEL: usize = 31 * 8;
 
-impl<'a, DB: KeyValueDb> ZkTriePersistentStorage<'a, DB> {
+impl<'a, DB: KeyValueDb> ZkTrieStateDb<'a, DB> {
     pub fn new(storage: &'a mut DB) -> Self {
         Self {
             storage: NodeDb(storage),
@@ -95,7 +98,7 @@ impl<'a, DB: KeyValueDb> ZkTriePersistentStorage<'a, DB> {
     }
 }
 
-impl<'a, DB: KeyValueDb> TrieDb for ZkTriePersistentStorage<'a, DB> {
+impl<'a, DB: KeyValueDb> StateDb for ZkTrieStateDb<'a, DB> {
     fn open(&mut self, root32: &[u8]) {
         self.trie = Some(ZkTrie::new(MAX_LEVEL, Hash::from_bytes(&root32)));
     }
@@ -131,6 +134,14 @@ impl<'a, DB: KeyValueDb> TrieDb for ZkTriePersistentStorage<'a, DB> {
         }
     }
 
+    fn get_code(&self, key: &[u8]) -> Option<Vec<u8>> {
+        self.storage.get_code(key)
+    }
+
+    fn set_code(&mut self, key: &[u8], code: &[u8]) {
+        self.storage.set_code(key, &code.to_vec())
+    }
+
     fn update(
         &mut self,
         key: &[u8],
@@ -158,7 +169,7 @@ impl<'a, DB: KeyValueDb> TrieDb for ZkTriePersistentStorage<'a, DB> {
 
 #[cfg(test)]
 mod tests {
-    use crate::storage::{zktrie::ZkTriePersistentStorage, InMemoryDb, TrieDb};
+    use crate::storage::{zktrie::ZkTrieStateDb, InMemoryDb, StateDb};
 
     macro_rules! bytes32 {
         ($val:expr) => {{
@@ -176,7 +187,7 @@ mod tests {
     fn test_simple() {
         let mut db = InMemoryDb::default();
         // create new zkt
-        let mut zkt = ZkTriePersistentStorage::new(&mut db);
+        let mut zkt = ZkTrieStateDb::new_empty(&mut db);
         zkt.update(
             bytes32!("key1"),
             0,
@@ -186,7 +197,7 @@ mod tests {
         let root = zkt.compute_root();
         println!("root: {:?}", hex::encode(root));
         // open and read value
-        let zkt2 = ZkTriePersistentStorage::new_opened(&mut db, &root);
+        let zkt2 = ZkTrieStateDb::new_opened(&mut db, &root);
         let data = zkt2.get(bytes32!("key1")).unwrap();
         assert_eq!(data[0], *bytes32!("value1"));
         assert_eq!(data[1], *bytes32!("value2"));
