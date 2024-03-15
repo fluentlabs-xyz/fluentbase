abdb02b36ad308ff34b1b8f8348a3b29835d2f25
Stas Stepanov <stanislav@ankr.com>
2024-03-15 15:19:55 +0600

    wcl: create2 test

--- e2e/src/core/tests_wasm_runtime.rs
+++ e2e/src/core/tests_wasm_runtime.rs
@@ -2,14 +2,16 @@ use crate::{core::testing_utils::TestingContext, test_helpers::wasm2rwasm};
 use fluentbase_codec::Encoder;
 use fluentbase_core::{
     account::Account,
-    helpers::{calc_create_address, rwasm_exec},
+    helpers::{calc_create2_address, calc_create_address, rwasm_exec},
 };
 use fluentbase_core_api::{
     api::CoreInput,
     bindings::{
         WasmCallMethodInput,
+        WasmCreate2MethodInput,
         WasmCreateMethodInput,
         WASM_CALL_METHOD_ID,
+        WASM_CREATE2_METHOD_ID,
         WASM_CREATE_METHOD_ID,
     },
 };
@@ -60,9 +62,10 @@ fn test_wasm_create() {
 
     let wasm_bytecode = include_bytes!("../../../examples/bin/greeting-deploy.wasm");
 
-    let value = B256::left_padding_from(&hex!("1000"));
+    let create_value = B256::left_padding_from(&hex!("1000"));
     let gas_limit: u32 = 10_000_000;
-    let method_input = WasmCreateMethodInput::new(value.0, wasm_bytecode.to_vec(), gas_limit);
+    let method_input =
+        WasmCreateMethodInput::new(create_value.0, wasm_bytecode.to_vec(), gas_limit);
     let core_input = CoreInput::new(WASM_CREATE_METHOD_ID, method_input.encode_to_vec(0));
     let core_input_vec = core_input.encode_to_vec(0);
 
@@ -83,6 +86,70 @@ fn test_wasm_create() {
         .set_contract_code_size(wasm_bytecode.len() as u32)
         .set_block_hash(block_hash)
         .set_block_coinbase(block_coinbase)
+        .set_tx_caller(caller_address);
+    test_ctx.apply_ctx(Some(&mut runtime_ctx));
+
+    let import_linker = Runtime::<()>::new_sovereign_linker();
+    let mut output = test_ctx.run_rwasm_with_input(runtime_ctx, &import_linker, false, gas_limit);
+    assert_eq!(ExitCode::Ok.into_i32(), output.data().exit_code());
+    let output_vec = output.data().output();
+    println!("output_vec {:?}", output_vec);
+    assert!(output_vec.len() > 0);
+    let contract_address = Address::from_slice(output_vec);
+    println!("deployed contract_address {:x?}", contract_address);
+    assert_eq!(expected_contract_address, contract_address);
+
+    {
+        let mut runtime_ctx = RuntimeContext::new(&[]);
+        runtime_ctx.with_jzkt(jzkt.clone());
+        let mut test_ctx =
+            TestingContext::<(), { !IS_RUNTIME }>::new(false, Some(&mut runtime_ctx));
+        test_ctx.apply_ctx(Some(&mut runtime_ctx));
+        let account = Account::new_from_jzkt(&contract_address);
+        assert_eq!(178, account.load_source_bytecode().len());
+        assert_eq!(432, account.load_bytecode().len());
+    }
+}
+
+#[test]
+fn test_wasm_create2() {
+    let caller_address = address!("000000000000000000000000000000000000000c");
+    let caller_account = Account {
+        address: caller_address,
+        balance: U256::from_be_slice(1000000000u128.to_be_bytes().as_slice()),
+        ..Default::default()
+    };
+
+    let wasm_bytecode = include_bytes!("../../../examples/bin/greeting-deploy.wasm");
+    let mut wasm_bytecode_hash = B256::default();
+    keccak_hash::keccak_256(wasm_bytecode.as_ref(), wasm_bytecode_hash.as_mut_slice());
+
+    let create_value = B256::left_padding_from(&hex!("1000"));
+    let salt = B256::left_padding_from(&hex!("3749269486238462"));
+    let gas_limit: u32 = 10_000_000;
+    let method_input =
+        WasmCreate2MethodInput::new(create_value.0, salt.0, wasm_bytecode.to_vec(), gas_limit);
+    let core_input = CoreInput::new(WASM_CREATE2_METHOD_ID, method_input.encode_to_vec(0));
+    let core_input_vec = core_input.encode_to_vec(0);
+
+    let expected_contract_address =
+        calc_create2_address(&caller_address, &salt, &wasm_bytecode_hash);
+
+    const IS_RUNTIME: bool = true;
+    let contract_wasm_binary = include_bytes!("../../../crates/core/bin/wcl_contract.wasm");
+    let contract_rwasm_binary = wasm2rwasm(contract_wasm_binary.as_slice(), false);
+    let mut runtime_ctx = RuntimeContext::new(contract_rwasm_binary);
+    let mut test_ctx = TestingContext::<(), IS_RUNTIME>::new(true, Some(&mut runtime_ctx));
+    let jzkt = runtime_ctx.jzkt().unwrap();
+    test_ctx
+        .try_add_account(&caller_account)
+        .contract_input_wrapper
+        .set_journal_checkpoint(runtime_ctx.jzkt().unwrap().borrow_mut().checkpoint().into())
+        .set_contract_input(Bytes::copy_from_slice(&core_input_vec))
+        .set_contract_input_size(core_input_vec.len() as u32)
+        .set_contract_caller(caller_address)
+        .set_contract_bytecode(Bytes::copy_from_slice(wasm_bytecode))
+        .set_contract_code_size(wasm_bytecode.len() as u32)
         .set_tx_caller(caller_address);
     test_ctx.apply_ctx(Some(&mut runtime_ctx));
 
