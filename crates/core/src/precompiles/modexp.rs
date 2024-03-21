cba9b76cf6f8b09b84b3eba517ae2433d59e8771
Stas Stepanov <stanislav@ankr.com>
2024-03-21 18:45:06 +0600

    fix precompiles

--- crates/core/src/precompiles/modexp.rs
+++ crates/core/src/precompiles/modexp.rs
@@ -1,4 +1,6 @@
 use fluentbase_sdk::{evm::ExecutionContext, LowLevelAPI, LowLevelSDK};
+use fluentbase_types::ExitCode;
+use revm_interpreter::primitives::PrecompileError;
 
 pub fn deploy() {}
 
@@ -6,7 +8,17 @@ pub fn main() {
     let input = ExecutionContext::contract_input();
     let gas_limit = ExecutionContext::contract_gas_limit();
 
-    let pr = revm_precompile::modexp::berlin_run(&input, gas_limit);
-    let (_, res) = pr.unwrap();
-    LowLevelSDK::sys_write(res.as_ref());
+    let result = revm_precompile::modexp::berlin_run(&input, gas_limit);
+    let result = match result {
+        Ok((_, result)) => result,
+        Err(err) => match err {
+            PrecompileError::OutOfGas => {
+                LowLevelSDK::sys_halt(ExitCode::OutOfFuel.into_i32());
+            }
+            _ => {
+                LowLevelSDK::sys_halt(ExitCode::PrecompileError.into_i32());
+            }
+        },
+    };
+    LowLevelSDK::sys_write(result.as_ref());
 }
