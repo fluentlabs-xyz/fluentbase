b9b3ef6ad584019ff47f2256b791812977e37f70
Stas Stepanov <stanislav@ankr.com>
2024-03-21 19:14:47 +0600

    fix secp256k1

--- crates/core/src/precompiles/secp256k1.rs
+++ crates/core/src/precompiles/secp256k1.rs
@@ -1,32 +1,30 @@
-use fluentbase_sdk::{LowLevelAPI, LowLevelSDK};
+use fluentbase_sdk::{
+    evm::{Bytes, ExecutionContext},
+    LowLevelAPI,
+    LowLevelSDK,
+};
 use fluentbase_types::ExitCode;
+use revm_interpreter::primitives::PrecompileError;
 
 pub fn deploy() {}
 
 pub fn main() {
-    const DIGEST_OFFSET: usize = 0;
-    const DIGEST_LEN: usize = 32;
-    const SIG_OFFSET: usize = DIGEST_OFFSET + DIGEST_LEN;
-    const SIG_LEN: usize = 64;
-    const REC_ID_OFFSET: usize = SIG_OFFSET + SIG_LEN;
-    const REC_ID_LEN: usize = 1;
-    const PK_EXPECTED_OFFSET: usize = REC_ID_OFFSET + REC_ID_LEN;
-    const PK_EXPECTED_LEN: usize = 65;
+    let input = ExecutionContext::contract_input();
+    let gas_limit = ExecutionContext::contract_gas_limit();
 
-    let mut digest = [0u8; DIGEST_LEN];
-    LowLevelSDK::sys_read(&mut digest, DIGEST_OFFSET as u32);
-    let mut sig = [0u8; SIG_LEN];
-    LowLevelSDK::sys_read(&mut sig, SIG_OFFSET as u32);
-    let mut rec_id = [0u8; REC_ID_LEN];
-    LowLevelSDK::sys_read(&mut rec_id, REC_ID_OFFSET as u32);
-    let mut pk_expected = [0u8; PK_EXPECTED_LEN];
-    LowLevelSDK::sys_read(&mut pk_expected, PK_EXPECTED_OFFSET as u32);
-    let mut pk_output = [0u8; PK_EXPECTED_LEN];
-
-    LowLevelSDK::crypto_ecrecover(&digest, &sig, &mut pk_output, rec_id[0]);
-    if pk_expected != pk_output {
-        LowLevelSDK::sys_halt(ExitCode::PrecompileError.into_i32());
-        return;
-    }
-    LowLevelSDK::sys_write(pk_output.as_ref());
+    let result = revm_precompile::secp256k1::ec_recover_run(&input, gas_limit);
+    let result = match result {
+        Ok((_, result)) => result,
+        Err(err) => match err {
+            PrecompileError::OutOfGas => {
+                LowLevelSDK::sys_halt(ExitCode::OutOfFuel.into_i32());
+                Bytes::new()
+            }
+            _ => {
+                LowLevelSDK::sys_halt(ExitCode::PrecompileError.into_i32());
+                Bytes::new()
+            }
+        },
+    };
+    LowLevelSDK::sys_write(result.as_ref());
 }
