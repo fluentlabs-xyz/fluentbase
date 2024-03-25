2e424abbd229850afa880f111a90a32ff3376972
Stas Stepanov <stanislav@ankr.com>
2024-03-25 13:12:48 +0600

    move example contract address

--- crates/genesis/src/lib.rs
+++ crates/genesis/src/lib.rs
@@ -1,3 +1,7 @@
 pub use alloy_genesis::{ChainConfig, Genesis, GenesisAccount};
+use fluentbase_types::{address, Address};
 
 pub mod devnet;
+
+// example
+pub const EXAMPLE_GREETING_ADDRESS: Address = address!("5300000000000000000000000000000000000001");
