a2ad43bba0cab922791de4a4e78173662a50dcef
Stas Stepanov <stanislav@ankr.com>
2024-04-17 16:25:31 +0600

    rwasm compile: fix bugs

--- crates/runtime/src/instruction/wasm_to_rwasm.rs
+++ crates/runtime/src/instruction/wasm_to_rwasm.rs
@@ -12,16 +12,25 @@ impl WasmToRwasm {
         output_offset: u32,
         output_len: u32,
     ) -> Result<i32, Trap> {
-        let wasm_binary = caller.read_memory(input_offset, input_len)?;
-        let rwasm_binary = Self::fn_impl(wasm_binary).map_err(|v| v.into_trap())?;
+        let wasm_binary = caller.read_memory(input_offset, input_len)?.to_vec();
+        let rwasm_binary = Self::fn_impl(caller.data_mut(), &wasm_binary, output_len)
+            .map_err(|v| v.into_trap())?;
         if output_len > 0 {
             caller.write_memory(output_offset, &rwasm_binary[0..output_len as usize])?;
         }
-        caller.data_mut().execution_result.return_data = rwasm_binary;
         Ok(ExitCode::Ok.into_i32())
     }
 
-    pub fn fn_impl(wasm_binary: &[u8]) -> Result<Vec<u8>, ExitCode> {
-        wasm2rwasm(wasm_binary)
+    pub fn fn_impl<DB: IJournaledTrie>(
+        ctx: &mut RuntimeContext<DB>,
+        wasm_binary: &[u8],
+        output_len: u32,
+    ) -> Result<Vec<u8>, ExitCode> {
+        let rwasm_binary = wasm2rwasm(wasm_binary)?;
+        if output_len > 0 && output_len < rwasm_binary.len() as u32 {
+            return Err(ExitCode::OutputOverflow);
+        }
+        ctx.execution_result.return_data = rwasm_binary.clone();
+        Ok(rwasm_binary)
     }
 }
