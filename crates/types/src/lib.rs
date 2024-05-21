63a30ba6e9f970a0cc3bad6546acc9d31f70d5bd
Dmitry Savonin <nwtsoas@gmail.com>
2024-05-21 18:23:40 +0400

    cleanup deps, fixed shakmaty deps
    Signed-off-by: Dmitry Savonin <nwtsoas@gmail.com>

--- crates/types/src/lib.rs
+++ crates/types/src/lib.rs
@@ -1,5 +1,6 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 #![allow(dead_code, unreachable_patterns, unused_macros, unused_imports)]
+#![warn(unused_crate_dependencies)]
 
 extern crate alloc;
 extern crate core;
