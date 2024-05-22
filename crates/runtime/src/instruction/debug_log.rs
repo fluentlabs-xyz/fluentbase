5fc8ab21748e90f17a8264ec00ee045ced8b7027
Stas Stepanov <stanislav@ankr.com>
2024-05-22 18:16:31 +0600

    fix: restore debug_log, fix makefile

--- crates/runtime/src/instruction/debug_log.rs
+++ crates/runtime/src/instruction/debug_log.rs
@@ -21,27 +21,27 @@ impl DebugLog {
     }
 
     pub fn fn_impl(msg: &[u8]) {
-        // let now = chrono::offset::Utc::now();
-        // let last_time = LAST_LOG_TIME.get();
-        // let curr_time = now.timestamp_millis();
-        // let time_diff = if last_time > 0 {
-        //     curr_time - last_time
-        // } else {
-        //     0
-        // };
-        // LAST_LOG_TIME.set(curr_time);
-        // // let now_str = now.format("%Y%m%d_%H%M%S%.3f");
-        // let msg = if msg.len() > 1000 {
-        //     &msg[..1000]
-        // } else {
-        //     &msg[..]
-        // };
-        // println!(
-        //     "debug_log (diff {}ms): {}",
-        //     time_diff,
-        //     std::str::from_utf8(msg)
-        //         .map(|s| s.to_string())
-        //         .unwrap_or_else(|_| { hex::encode(msg) })
-        // );
+        let now = chrono::offset::Utc::now();
+        let last_time = LAST_LOG_TIME.get();
+        let curr_time = now.timestamp_millis();
+        let time_diff = if last_time > 0 {
+            curr_time - last_time
+        } else {
+            0
+        };
+        LAST_LOG_TIME.set(curr_time);
+        // let now_str = now.format("%Y%m%d_%H%M%S%.3f");
+        let msg = if msg.len() > 1000 {
+            &msg[..1000]
+        } else {
+            &msg[..]
+        };
+        println!(
+            "debug_log (diff {}ms): {}",
+            time_diff,
+            std::str::from_utf8(msg)
+                .map(|s| s.to_string())
+                .unwrap_or_else(|_| { hex::encode(msg) })
+        );
     }
 }
