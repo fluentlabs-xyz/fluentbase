5080e678e9ce5c0c3a466216fea0764a40125c09
d1r1 <me@d1r1.me>
2024-05-30 13:40:47 +0400

    add panic

---
+++ examples/projects/panic/lib.rs
@@ -0,0 +1,21 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+#![allow(dead_code)]
+
+extern crate alloc;
+extern crate fluentbase_sdk;
+
+use fluentbase_sdk::{LowLevelAPI, LowLevelSDK};
+
+// Function to deploy the contract
+#[cfg(not(feature = "std"))]
+#[no_mangle]
+#[cfg(target_arch = "wasm32")]
+pub extern "C" fn deploy() {}
+
+// Main function
+#[cfg(not(feature = "std"))]
+#[no_mangle]
+#[cfg(target_arch = "wasm32")]
+pub extern "C" fn main() {
+    panic!("it is panic time")
+}
