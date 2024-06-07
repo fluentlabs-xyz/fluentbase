2c98853641f8822ce686d0655ffa46b5deaf3f66
d1r1 <me@d1r1.me>
2024-06-07 16:12:49 +0400

    refactor solidity router and get rid of alloc::Vec

--- crates/sdk-derive/src/lib.rs
+++ crates/sdk-derive/src/lib.rs
@@ -1,23 +1,14 @@
-use convert_case::{Case, Casing};
 use proc_macro::TokenStream;
-use quote::{quote, ToTokens};
+
+use convert_case::{Case, Casing};
+use quote::quote;
 use syn::{
     self,
     parse::{Parse, ParseStream},
     parse_macro_input,
     punctuated::Punctuated,
-    Expr,
-    ExprLit,
-    FnArg,
-    Ident,
-    ImplItem,
-    ItemImpl,
-    Lit,
-    Meta,
-    Token,
-    Type,
-    TypePath,
-    Visibility,
+    Expr, ExprLit, FnArg, Ident, ImplItem, ImplItemFn, ItemImpl, Lit, LitStr, Meta, Token, Type,
+    TypePath, Visibility,
 };
 
 #[proc_macro]
@@ -67,6 +58,12 @@ impl Parse for SolidityRouterInput {
     }
 }
 
+// Fake implementation of the attribute to avoid compiler and linter complaints
+#[proc_macro_attribute]
+pub fn sol_signature(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    item
+}
+
 #[proc_macro_attribute]
 pub fn derive_solidity_router(attr: TokenStream, item: TokenStream) -> TokenStream {
     let input = parse_macro_input!(attr as SolidityRouterInput);
@@ -74,63 +71,116 @@ pub fn derive_solidity_router(attr: TokenStream, item: TokenStream) -> TokenStre
 
     let struct_name = &ast.self_ty;
 
-    let router_match_arms: Vec<_> = ast
-        .items
-        .iter()
-        .filter_map(expand_router_match_arm)
-        .collect();
-
-    let methods: Vec<_> = ast
-        .items
-        .iter()
-        .filter(|item| matches!(item, ImplItem::Fn(_)))
-        .collect();
-
-    let expanded = expand_solidity_router(struct_name, router_match_arms, methods, input.with_main);
-
-    TokenStream::from(expanded)
-}
-
-fn expand_solidity_router(
-    struct_name: &Box<Type>,
-    router_match_arms: Vec<proc_macro2::TokenStream>,
-    methods: Vec<&ImplItem>,
-    with_main: bool,
-) -> proc_macro2::TokenStream {
-    let router = expand_router(struct_name, router_match_arms, methods.clone());
-    let sol_signatures = expand_sol(methods);
-
-    let main_fn = if with_main {
-        expand_main_fn(struct_name)
+    // Get all public methods from the impl block
+    let methods_to_route = get_methods_to_route(&ast);
+    // Generate Solidity function signatures or use provided ones from #[sol_signature]
+    let sol_signatures = get_sol_signatures(&methods_to_route);
+    // Derive route method that dispatches Solidity function calls
+    let router = derive_route_method(struct_name, &methods_to_route);
+    // Generate main function that is used in the contract entry point
+    let main_fn = if input.with_main {
+        derive_main_fn(struct_name)
     } else {
         quote! {}
     };
 
-    quote! {
+    let expanded = quote! {
         #router
         #sol_signatures
         #main_fn
+    };
+
+    TokenStream::from(expanded)
+}
+
+fn get_methods_to_route(ast: &ItemImpl) -> Vec<&syn::ImplItemFn> {
+    let all_methods: Vec<_> = ast
+        .items
+        .iter()
+        .filter_map(|item| {
+            if let ImplItem::Fn(func) = item {
+                Some(func)
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    let public_methods: Vec<&ImplItemFn> = all_methods
+        .into_iter()
+        .filter(|func| matches!(func.vis, Visibility::Public(_)))
+        .collect();
+
+    public_methods
+}
+
+fn get_sol_signatures(methods: &[&ImplItemFn]) -> proc_macro2::TokenStream {
+    let mut signatures: Vec<proc_macro2::TokenStream> = vec![];
+    for func in methods {
+        let sig: Option<LitStr> = func.attrs.iter().find_map(|attr| {
+            if attr.path().is_ident("sol_signature") {
+                attr.parse_args().ok()
+            } else {
+                None
+            }
+        });
+
+        if let Some(fn_signature) = sig {
+            let fn_signature = fn_signature.value() + "; ";
+            let fn_signature = syn::parse_str::<proc_macro2::TokenStream>(&fn_signature)
+                .expect("Failed to parse signature");
+            signatures.push(fn_signature);
+        } else {
+            let method_name = &func.sig.ident;
+            let sol_method_name = rust_name_to_sol(method_name);
+
+            // Collect input parameter types and names
+            let inputs = parse_function_inputs(&func.sig.inputs);
+
+            // Collect output parameter type
+            let output = if let syn::ReturnType::Type(_, ty) = &func.sig.output {
+                rust_type_to_sol(ty)
+            } else {
+                quote! { void }
+            };
+
+            // Generate function signature in Solidity syntax
+            signatures.push(quote! {
+                function #sol_method_name(#(#inputs),*) external view returns (#output);
+            });
+        }
+    }
+    quote! {
+        sol! {
+            #(#signatures)*
+        }
     }
 }
 
-fn expand_router(
+fn derive_route_method(
     struct_name: &Box<Type>,
-    router_match_arms: Vec<proc_macro2::TokenStream>,
-    methods: Vec<&ImplItem>,
+    methods: &Vec<&ImplItemFn>,
 ) -> proc_macro2::TokenStream {
-    quote! {
+    let selectors: Vec<proc_macro2::TokenStream> = methods
+        .iter()
+        .filter_map(|method| {
+            let selector = derive_route_selector_arm(method);
+            Some(selector)
+        })
+        .collect();
 
+    quote! {
         impl #struct_name {
             #(#methods)*
 
-            pub fn route(&self, input: &alloc::vec::Vec<u8>) -> alloc::vec::Vec<u8> {
+            pub fn route(&self, input: &[u8], output: &mut [u8]) {
                 if input.len() < 4 {
                     panic!("input too short, cannot extract selector");
                 }
                 let mut selector: [u8; 4] = [0; 4];
                 selector.copy_from_slice(&input[0..4]);
                 match selector {
-                    #(#router_match_arms),*,
+                    #(#selectors),*,
                     _ => panic!("unknown method"),
                 }
             }
@@ -138,7 +188,72 @@ fn expand_router(
     }
 }
 
-fn expand_main_fn(struct_name: &Box<Type>) -> proc_macro2::TokenStream {
+fn derive_route_selector_arm(func: &ImplItemFn) -> proc_macro2::TokenStream {
+    let method_name = &func.sig.ident;
+    let method_name_call = sol_call_fn_name(method_name);
+    let selector_name = quote! { #method_name_call::SELECTOR };
+    let abi_decode = quote! { #method_name_call::abi_decode };
+
+    let args: Vec<_> = func
+        .sig
+        .inputs
+        .iter()
+        .filter_map(|arg| {
+            if let FnArg::Typed(pat_type) = arg {
+                if let syn::Pat::Ident(pat_ident) = &*pat_type.pat {
+                    Some(&pat_ident.ident)
+                } else {
+                    None
+                }
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    let args_expr = derive_route_selector_args(&args, &abi_decode);
+
+    quote! {
+        #selector_name => {
+            #args_expr
+            let encoded_output = self.#method_name(#(#args),*).abi_encode();
+            if encoded_output.len() > output.len() {
+                panic!("output buffer too small");
+            };
+            output[..encoded_output.len()].copy_from_slice(&encoded_output);
+        }
+    }
+}
+
+fn derive_route_selector_args(
+    args: &[&Ident],
+    abi_decode_fn: &proc_macro2::TokenStream,
+) -> proc_macro2::TokenStream {
+    if args.len() == 1 {
+        let arg = args[0];
+        quote! {
+            let #arg = match #abi_decode_fn(&input, true) {
+                Ok(decoded) => decoded.#arg,
+                Err(e) => {
+                    panic!("Failed to decode input {:?} {:?}", stringify!(#arg), e);
+                }
+            };
+        }
+    } else {
+        let fields: Vec<proc_macro2::TokenStream> =
+            args.iter().map(|arg| quote! { decoded.#arg }).collect();
+        quote! {
+            let (#(#args),*) = match #abi_decode_fn(&input, true) {
+                Ok(decoded) => (#(#fields),*),
+                Err(e) => {
+                    panic!("Failed to decode input {:?}", e);
+                }
+            };
+        }
+    }
+}
+
+fn derive_main_fn(struct_name: &Box<Type>) -> proc_macro2::TokenStream {
     quote! {
         #[cfg(not(feature = "std"))]
         #[no_mangle]
@@ -154,115 +269,6 @@ fn expand_main_fn(struct_name: &Box<Type>) -> proc_macro2::TokenStream {
             LowLevelSDK::sys_write(&output);
         }
     }
-}
-
-fn expand_router_match_arm(item: &ImplItem) -> Option<proc_macro2::TokenStream> {
-    if let ImplItem::Fn(func) = item {
-        if let Visibility::Public(_) = func.vis {
-            let method_name = &func.sig.ident;
-            let method_name_call = sol_call_fn_name(method_name);
-            let selector_name = quote! { #method_name_call::SELECTOR };
-            let abi_decode = quote! { #method_name_call::abi_decode };
-
-            let param_names: Vec<_> = func
-                .sig
-                .inputs
-                .iter()
-                .filter_map(|arg| {
-                    if let FnArg::Typed(pat_type) = arg {
-                        if let syn::Pat::Ident(pat_ident) = &*pat_type.pat {
-                            Some(&pat_ident.ident)
-                        } else {
-                            None
-                        }
-                    } else {
-                        None
-                    }
-                })
-                .collect();
-
-            let args_expr = expand_args_expr(&abi_decode, &param_names);
-
-            return Some(quote! {
-                #selector_name => {
-                    #args_expr
-                    self.#method_name(#(#param_names),*).abi_encode()
-                }
-            });
-        }
-    }
-    None
-}
-
-fn expand_args_expr(
-    abi_decode: &proc_macro2::TokenStream,
-    param_names: &[&Ident],
-) -> proc_macro2::TokenStream {
-    if param_names.len() == 1 {
-        let param_name = &param_names[0];
-        quote! {
-            let #param_name = match #abi_decode(&input, true) {
-                Ok(decoded) => decoded.#param_name,
-                Err(e) => {
-                    panic!("Failed to decode input {:?} {:?}", stringify!(#param_name), e);
-                }
-            };
-        }
-    } else {
-        let fields: Vec<proc_macro2::TokenStream> = param_names
-            .iter()
-            .map(|param_name| quote! { decoded.#param_name })
-            .collect();
-        quote! {
-            let (#(#param_names),*) = match #abi_decode(&input, true) {
-                Ok(decoded) => (#(#fields),*),
-                Err(e) => {
-                    panic!("Failed to decode input {:?}", e);
-                }
-            };
-        }
-    }
-}
-
-fn expand_sol(methods: Vec<&ImplItem>) -> proc_macro2::TokenStream {
-    let sol_functions: Vec<_> = methods
-        .iter()
-        .filter_map(|item| {
-            if let ImplItem::Fn(func) = item {
-                if let Visibility::Public(_) = func.vis {
-                    let method_name = &func.sig.ident;
-                    let sol_method_name = rust_name_to_sol(method_name);
-
-                    // Collect input parameter types and names
-                    let inputs = parse_function_inputs(&func.sig.inputs);
-
-                    // Collect output parameter type
-                    let output = if let syn::ReturnType::Type(_, ty) = &func.sig.output {
-                        rust_type_to_sol(ty)
-                    } else {
-                        quote! { void }
-                    };
-
-                    // Generate function signature in Solidity syntax
-                    Some(quote! {
-                        function #sol_method_name(#(#inputs),*) external view returns (#output);
-                    })
-                } else {
-                    None
-                }
-            } else {
-                None
-            }
-        })
-        .collect();
-
-    let sol_block = quote! {
-        sol! {
-            #(#sol_functions)*
-        }
-    };
-
-    sol_block
 }
 
 fn parse_function_inputs(inputs: &Punctuated<FnArg, Token![,]>) -> Vec<proc_macro2::TokenStream> {
@@ -385,26 +391,13 @@ fn convert_path_type(type_path: &TypePath) -> proc_macro2::TokenStream {
 
 #[cfg(test)]
 mod tests {
+    use syn::{parse_quote, Ident, TypeArray, TypeParen, TypePath, TypeSlice, TypeTuple};
+
     use super::*;
-    use syn::{parse_quote, Ident, ImplItem, TypeArray, TypeParen, TypePath, TypeSlice, TypeTuple};
 
     #[test]
-    fn test_rust_name_to_sol() {
-        let ident = Ident::new("test_function", proc_macro2::Span::call_site());
-        let sol_ident = rust_name_to_sol(&ident);
-        assert_eq!(sol_ident.to_string(), "testFunction");
-    }
-
-    #[test]
-    fn test_get_method_call() {
-        let method_name = Ident::new("test_function", proc_macro2::Span::call_site());
-        let method_call_ident = sol_call_fn_name(&method_name);
-        assert_eq!(method_call_ident.to_string(), "testFunctionCall");
-    }
-
-    #[test]
-    fn test_expand_match_arm_single_param() {
-        let item: ImplItem = parse_quote! {
+    fn test_derive_route_selector_arm_single_param() {
+        let item: ImplItemFn = parse_quote! {
             pub fn is_checkmate(&self, board: String) -> bool {
                 true
             }
@@ -422,16 +415,13 @@ mod tests {
             }
         };
 
-        if let Some(actual) = expand_router_match_arm(&item) {
-            assert_eq!(actual.to_string(), expected.to_string());
-        } else {
-            panic!("Failed to expand match arm");
-        }
+        let actual = derive_route_selector_arm(&item);
+        assert_eq!(actual.to_string(), expected.to_string());
     }
 
     #[test]
-    fn test_expand_match_arm_multiple_params() {
-        let item: ImplItem = parse_quote! {
+    fn test_derive_route_selector_arm_multiple_params() {
+        let item: ImplItemFn = parse_quote! {
             pub fn is_checkmate(&self, board: String, mv: String) -> bool {
                 true
             }
@@ -449,50 +439,54 @@ mod tests {
             }
         };
 
-        if let Some(actual) = expand_router_match_arm(&item) {
-            assert_eq!(actual.to_string(), expected.to_string());
-        } else {
-            panic!("Failed to expand match arm");
-        }
+        let actual = derive_route_selector_arm(&item);
+        assert_eq!(actual.to_string(), expected.to_string());
     }
 
     #[test]
-    fn test_expand_args_expr_single_param() {
-        let abi_decode: proc_macro2::TokenStream = quote! { isCheckmateCall::abi_decode };
-        let param_board = Ident::new("board", proc_macro2::Span::call_site());
-        let param_names = vec![&param_board];
-
-        let args_expr = expand_args_expr(&abi_decode, &param_names);
-        let expected = quote! {
-            let board = match isCheckmateCall::abi_decode(&input, true) {
-                Ok(decoded) => decoded.board,
-                Err(e) => {
-                    panic!("Failed to decode input {:?} {:?}", stringify!(board), e);
+    fn test_get_sol_signatures() {
+        let item_impl: ItemImpl = parse_quote! {
+            impl ExampleStruct {
+                #[sol_signature("function greeting() external view returns ()")]
+                pub fn greeting(&self, msg: String) -> String {
+                    msg
                 }
-            };
+
+                pub fn regular_fn(&self, msg: String) -> String {
+                    msg
+                }
+                pub fn greeting_msg(&self, msg: String) -> String {
+                    msg
+                }
+            }
         };
 
-        assert_eq!(args_expr.to_string(), expected.to_string());
+        let methods = get_methods_to_route(&item_impl);
+        let sol_signatures = get_sol_signatures(&methods);
+
+        let expected = quote! {
+            sol! {
+                function greeting() external view returns ();
+                function regularFn(string msg) external view returns (string);
+                function greetingMsg(string msg) external view returns (string);
+            }
+        };
+
+        assert_eq!(sol_signatures.to_string(), expected.to_string());
     }
 
     #[test]
-    fn test_expand_args_expr_multiple_params() {
-        let abi_decode: proc_macro2::TokenStream = quote! { isCheckmateCall::abi_decode };
-        let param_board = Ident::new("board", proc_macro2::Span::call_site());
-        let param_mv = Ident::new("mv", proc_macro2::Span::call_site());
-        let param_names = vec![&param_board, &param_mv];
+    fn test_rust_name_to_sol() {
+        let ident = Ident::new("test_function", proc_macro2::Span::call_site());
+        let sol_ident = rust_name_to_sol(&ident);
+        assert_eq!(sol_ident.to_string(), "testFunction");
+    }
 
-        let args_expr = expand_args_expr(&abi_decode, &param_names);
-        let expected = quote! {
-            let (board, mv) = match isCheckmateCall::abi_decode(&input, true) {
-                Ok(decoded) => (decoded.board, decoded.mv),
-                Err(e) => {
-                    panic!("Failed to decode input {:?}", e);
-                }
-            };
-        };
-
-        assert_eq!(args_expr.to_string(), expected.to_string());
+    #[test]
+    fn test_get_method_call() {
+        let method_name = Ident::new("test_function", proc_macro2::Span::call_site());
+        let method_call_ident = sol_call_fn_name(&method_name);
+        assert_eq!(method_call_ident.to_string(), "testFunctionCall");
     }
 
     #[test]
