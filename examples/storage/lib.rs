2e0876d9c7e8e3ac009c676c3672ad5397606665
d1r1 <me@d1r1.me>
2024-06-19 17:03:32 +0200

    add test for mapping key

---
+++ examples/storage/lib.rs
@@ -0,0 +1,70 @@
+#![cfg_attr(target_arch = "wasm32", no_std)]
+extern crate alloc;
+extern crate fluentbase_sdk;
+
+use alloy_sol_types::{sol, SolValue};
+use fluentbase_sdk::{LowLevelSDK, derive::solidity_storage, Address, SharedAPI, U256};
+
+
+
+solidity_storage! {
+    U256[] Arr;
+    mapping(Address => mapping(Address => U256)) AllowanceStorage;
+    mapping(Address owner => mapping(Address users => mapping(Address balances => MyStruct))) BalancesStorage;
+}
+
+sol! {
+    contract HelloWorld {
+        U256[] arr;
+    }
+}
+
+pub fn mapping_key(slot: U256, key: U256) -> U256 {
+    let mut raw_storage_key: [u8; 64] = [0; 64];
+    raw_storage_key[0..32].copy_from_slice(slot.as_le_slice());
+    raw_storage_key[32..64].copy_from_slice(key.as_le_slice());
+    let mut storage_key: [u8; 32] = [0; 32];
+    LowLevelSDK::keccak256(
+        raw_storage_key.as_ptr(),
+        raw_storage_key.len() as u32,
+        storage_key.as_mut_ptr(),
+    );
+    U256::from_be_bytes(storage_key)
+}
+
+pub fn array_key(slot: U256, index: U256) -> U256 {
+    let mut storage_key: [u8; 32] = [0; 32];
+    LowLevelSDK::keccak256(slot.as_le_slice().as_ptr(), 32, storage_key.as_mut_ptr());
+    let storage_key = U256::from_be_bytes(storage_key);
+    storage_key + index
+}
+
+pub fn field_key(slot: U256) -> U256 {
+    slot
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use fluentbase_sdk::U256;
+
+    #[test]
+    fn test_mapping_storage() {
+        let owner = Address::default();
+        let spender = Address::default();
+        let _amount = U256::from(100);
+
+        let allowance_storage = AllowanceStorage{};
+        print!("{:?}", AllowanceStorage::SLOT);
+
+        let key = allowance_storage.key(owner, spender);
+        println!("{:?}", key);
+
+        let expected_key = U256::from_str_radix("45674214084458039979679588399856379396083966111810618268439781217138252554320",10)
+        .expect("failed to parse U256 from string");
+    assert_eq!(key, expected_key);
+
+    }
+}
