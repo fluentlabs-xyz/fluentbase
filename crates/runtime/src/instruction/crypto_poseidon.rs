71423b4939d48cba0db3d899ef2c778518d6dcfc
Eveniizh <eugene@fluentlabs.xyz>
2024-06-19 20:18:51 +0700

    clean some code

--- crates/runtime/src/instruction/crypto_poseidon.rs
+++ crates/runtime/src/instruction/crypto_poseidon.rs
@@ -10,16 +10,13 @@ impl CryptoPoseidon {
         f32s_len: u32,
         output_offset: u32,
     ) -> Result<(), Trap> {
-        println!("DEBUG POSEIDON 1 WE CALL fn handler");
         let data = caller.read_memory(f32s_offset, f32s_len);
-        println!("DEBUG WE DO MEMORY WRITE FOR POSEIDON 1 fn handler");
         caller.write_memory(output_offset, &Self::fn_impl(data));
         Ok(())
     }
 
     pub fn fn_impl(data: &[u8]) -> [u8; 32] {
         use fluentbase_poseidon::poseidon_hash;
-        println!("DEBUG FN_IMPL PREIMAGE {:#?}", &data);
         poseidon_hash(data)
     }
 }
