3e605df4e6dbc9257e43063a86879f220e68fc95
Dmitry Savonin <nwtsoas@gmail.com>
2024-06-22 23:47:45 +0400

    new evm/wasm loader and deployer precompiles, tiny fixes
    Signed-off-by: Dmitry Savonin <nwtsoas@gmail.com>

--- crates/contracts/src/wasm_loader.rs
+++ crates/contracts/src/wasm_loader.rs
@@ -1,42 +1,46 @@
+use fluentbase_core::wasm::call::_wasm_call;
 use fluentbase_sdk::{
     alloc_slice,
     basic_entrypoint,
-    contracts::{EvmAPI, EvmClient, PRECOMPILE_EVM},
     derive::Contract,
-    types::EvmCallMethodInput,
+    types::WasmCallMethodInput,
     AccountManager,
     ContextReader,
-    SharedAPI,
+    SovereignAPI,
 };
 use revm_precompile::Bytes;
 
 #[derive(Contract)]
-pub struct LOADER<'a, CR: ContextReader, AM: AccountManager> {
+pub struct WasmLoaderImpl<'a, CR: ContextReader, AM: AccountManager> {
     cr: &'a CR,
     am: &'a AM,
 }
 
-impl<'a, CR: ContextReader, AM: AccountManager> LOADER<'a, CR, AM> {
-    pub fn deploy<SDK: SharedAPI>(&self) {
+impl<'a, CR: ContextReader, AM: AccountManager> WasmLoaderImpl<'a, CR, AM> {
+    pub fn deploy<SDK: SovereignAPI>(&self) {
         unreachable!("deploy is not supported for loader")
     }
-    pub fn main<SDK: SharedAPI>(&self) {
+    pub fn main<SDK: SovereignAPI>(&self) {
         let input_size = SDK::input_size();
         let input = alloc_slice(input_size as usize);
         SDK::read(input.as_mut_ptr(), input_size, 0);
-        let evm_client = EvmClient::new(PRECOMPILE_EVM);
-        let output = evm_client.call(EvmCallMethodInput {
+        let input = WasmCallMethodInput {
             callee: self.cr.contract_address(),
             value: self.cr.contract_value(),
             input: Bytes::copy_from_slice(input),
             gas_limit: self.cr.contract_gas_limit(),
             depth: 0,
-        });
+        };
+        let output = _wasm_call(self.cr, self.am, input);
         SDK::write(output.output.as_ptr(), output.output.len() as u32);
         SDK::exit(output.exit_code);
     }
 }
 
 basic_entrypoint!(
-    LOADER<'static, fluentbase_sdk::GuestContextReader, fluentbase_sdk::GuestAccountManager>
+    WasmLoaderImpl<
+        'static,
+        fluentbase_sdk::GuestContextReader,
+        fluentbase_sdk::GuestAccountManager,
+    >
 );
