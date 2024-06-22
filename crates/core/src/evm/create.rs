82c00f5750567c2ff067bc2292236dd173b797d0
Dmitry Savonin <nwtsoas@gmail.com>
2024-06-22 23:47:45 +0400

    new evm/wasm loader and deployer precompiles, tiny fixes
    Signed-off-by: Dmitry Savonin <nwtsoas@gmail.com>

--- crates/core/src/evm/create.rs
+++ crates/core/src/evm/create.rs
@@ -132,13 +132,10 @@ pub fn _evm_create<CR: ContextReader, AM: AccountManager>(
             .with_gas(result.gas.remaining(), result.gas.refunded());
     }
 
-    let evm_rwasm_loader: Bytes =
-        include_bytes!("../../../contracts/assets/precompile_loader.rwasm").into();
-
     // write callee changes to a database (lets keep rWASM part empty for now since universal loader
     // is not ready yet)
     let (mut contract_account, _) = am.account(contract_account.address);
-    contract_account.update_bytecode(am, &result.output, None, &evm_rwasm_loader, None);
+    contract_account.update_bytecode(am, &result.output, None, &Bytes::new(), None);
 
     debug_log!(
         "ecl(_evm_create): return: Ok: callee_account.address: {}",
