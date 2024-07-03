61e82d88ead2e18e0e6eaec231a6662480710ebb
d1r1 <me@d1r1.me>
2024-07-03 17:39:28 +0200

    fix imports

--- examples/storage/lib.rs
+++ examples/storage/lib.rs
@@ -3,12 +3,8 @@ extern crate alloc;
 extern crate core;
 extern crate fluentbase_sdk;
 
-use crate::storage::StorageValue;
-use alloy_sol_types::SolValue;
-use core::{borrow::Borrow, fmt::Debug};
 use fluentbase_sdk::{
-    codec::{BufferDecoder, Encoder, WritableBuffer},
-    contracts::{EvmAPI, EvmClient, EvmSloadInput, EvmSstoreInput, PRECOMPILE_EVM},
+    codec::Codec,
     derive::solidity_storage,
     Address,
     Bytes,
@@ -17,86 +13,12 @@ use fluentbase_sdk::{
     U256,
 };
 
-mod storage;
-
-#[derive(Debug, Default, Clone, PartialEq)]
+#[derive(Codec, Debug, Default, Clone, PartialEq)]
 pub struct MyStruct {
     pub a: U256,
     pub b: U256,
-}
-
-impl Encoder<MyStruct> for MyStruct {
-    const HEADER_SIZE: usize = U256::HEADER_SIZE + U256::HEADER_SIZE;
-
-    fn encode<W: WritableBuffer>(&self, encoder: &mut W, field_offset: usize) {
-        self.a.encode(encoder, field_offset);
-        self.b.encode(encoder, field_offset + U256::HEADER_SIZE);
-    }
-
-    fn decode_header(
-        decoder: &mut BufferDecoder,
-        field_offset: usize,
-        result: &mut MyStruct,
-    ) -> (usize, usize) {
-        let (a_size, a_offset) = U256::decode_header(decoder, field_offset, &mut result.a);
-        let (b_size, b_offset) =
-            U256::decode_header(decoder, field_offset + a_offset, &mut result.b);
-        (a_size + b_size, a_offset + b_offset)
-    }
-
-    fn decode_body(decoder: &mut BufferDecoder, field_offset: usize, result: &mut MyStruct) {
-        let (_, _) = U256::decode_header(decoder, field_offset, &mut result.a);
-        U256::decode_body(decoder, field_offset, &mut result.a);
-
-        U256::decode_body(decoder, 32, &mut result.b);
-    }
-}
-
-impl<Client: EvmAPI> StorageValue<Client, MyStruct> for MyStruct {
-    fn get(&self, client: &Client, key: U256) -> Result<MyStruct, String> {
-        let chunk_size = 32;
-        let num_chunks = 2;
-
-        let mut buffer = Vec::with_capacity(num_chunks * chunk_size);
-        for i in 0..num_chunks {
-            let input = EvmSloadInput {
-                index: key + U256::from(i),
-            };
-            let output = client.sload(input);
-            let chunk = output.value.to_be_bytes::<32>();
-            buffer.extend_from_slice(&chunk);
-        }
-
-        let mut decoder = BufferDecoder::new(&buffer);
-        let mut body = MyStruct::default();
-        MyStruct::decode_body(&mut decoder, 0, &mut body);
-        Ok(body)
-    }
-
-    fn set(&self, client: &Client, key: U256, value: MyStruct) -> Result<(), String> {
-        let encoded_buffer = value.encode_to_vec(0);
-        let chunk_size = 32;
-        let num_chunks = (encoded_buffer.len() + chunk_size - 1) / chunk_size;
-
-        for i in 0..num_chunks {
-            let start = i * chunk_size;
-            let end = (start + chunk_size).min(encoded_buffer.len());
-            let chunk = &encoded_buffer[start..end];
-
-            let mut chunk_padded = [0u8; 32];
-            chunk_padded[..chunk.len()].copy_from_slice(chunk);
-
-            let value_u256 = U256::from_be_bytes(chunk_padded);
-            let input = EvmSstoreInput {
-                index: key + U256::from(i),
-                value: value_u256,
-            };
-
-            client.sstore(input);
-        }
-
-        Ok(())
-    }
+    pub c: Bytes,
+    pub d: Bytes,
 }
 
 solidity_storage! {
@@ -107,6 +29,7 @@ solidity_storage! {
     Address Owner;
     Bytes Data;
     MyStruct SomeStruct;
+    mapping(Address => MyStruct) MyStructMap;
 }
 
 #[cfg(test)]
@@ -125,9 +48,6 @@ mod test {
         LowLevelSDK::with_test_input(input.into());
     }
 
-    fn get_output() -> Vec<u8> {
-        LowLevelSDK::get_test_output()
-    }
     #[serial]
     #[test]
     pub fn test_primitive_storage_dynamic_bytes() {
@@ -135,8 +55,8 @@ mod test {
         with_test_input(vec![], None);
 
         let b = fluentbase_sdk::Bytes::from(
-            "this it really long string. this it really long
-    string. this it really long string. this it really long string.",
+            "this is a really long string. this is a really long
+    string. this is a really long string. this it really long string.",
         );
         let storage = Data::default();
         storage.set(b.clone());
@@ -225,13 +145,45 @@ mod test {
 
         let a = U256::from(1);
         let b = U256::from(2);
+        let c = fluentbase_sdk::Bytes::from(
+            "this it really long string. this it really long
+    string. this it really long string. this it really long string.",
+        );
+        let d = fluentbase_sdk::Bytes::from("short");
 
-        let my_struct = MyStruct { a, b };
+        let my_struct = MyStruct { a, b, c, d };
 
         storage.set(my_struct.clone());
 
         let result = storage.get();
 
         assert_eq!(result, my_struct);
+    }
+
+    #[serial]
+    #[test]
+    pub fn test_storage_mapping_struct() {
+        LowLevelSDK::init_with_devnet_genesis();
+        with_test_input(vec![], None);
+
+        let storage = MyStructMap::default();
+
+        let addr = Address::from(hex!("f39Fd6e51aad88F6F4ce6aB8827279cffFb92266"));
+
+        let a = U256::from(1);
+        let b = U256::from(2);
+        let c = fluentbase_sdk::Bytes::from(
+            "this it really long string. this it really long
+    string. this it really long string. this it really long string.",
+        );
+        let d = fluentbase_sdk::Bytes::from("short");
+
+        let my_struct = MyStruct { a, b, c, d };
+
+        storage.set(addr, my_struct.clone());
+
+        let result = storage.get(addr);
+
+        assert_eq!(result, my_struct.clone());
     }
 }
