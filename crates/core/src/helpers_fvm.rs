d7e96866167983c898cf72c47cf0d5969c4e9275
Stas Stepanov <stanislav@ankr.com>
2024-07-30 20:16:17 +0600

    fuel fixes after unit-tests

--- crates/core/src/helpers_fvm.rs
+++ crates/core/src/helpers_fvm.rs
@@ -1,9 +1,13 @@
-use crate::fvm::types::WasmRelayer;
-use alloc::vec::Vec;
+use crate::{
+    debug_log,
+    fvm::types::{WasmRelayer, WasmStorage},
+};
+use alloc::{borrow::ToOwned, vec::Vec};
+use fluentbase_sdk::{AccountManager, ContextReader};
 use fuel_core_executor::executor::{BlockExecutor, ExecutionOptions, TxStorageTransaction};
 use fuel_core_storage::{
     column::Column,
-    kv_store::KeyValueInspect,
+    kv_store::{KeyValueInspect, KeyValueMutate, WriteOperation},
     structured_storage::StructuredStorage,
     transactional::{Changes, ConflictPolicy, InMemoryTransaction, IntoTransaction},
 };
@@ -26,19 +30,20 @@ pub fn fvm_transact<'a, Tx, T>(
     gas_price: Word,
     memory: &'a mut MemoryInstance,
     consensus_params: ConsensusParameters,
+    extra_tx_checks: bool,
 ) -> Result<(bool, ProgramState, Tx, Vec<Receipt>, Changes)>
 where
-    Tx: ExecutableTransaction + Cacheable,
+    Tx: ExecutableTransaction + Cacheable + Send + Sync + 'static,
     <Tx as IntoChecked>::Metadata: CheckedMetadata + Send + Sync,
     T: KeyValueInspect<Column = Column>,
 {
     let execution_options = ExecutionOptions {
-        extra_tx_checks: true,
+        extra_tx_checks,
         backtrace: false,
     };
 
     let mut block_executor =
-        BlockExecutor::new(WasmRelayer {}, execution_options, consensus_params)
+        BlockExecutor::new(WasmRelayer {}, execution_options.clone(), consensus_params)
             .expect("failed to create block executor");
 
     let structured_storage = StructuredStorage::new(storage);
@@ -48,20 +53,110 @@ where
         ConflictPolicy::Overwrite,
         &mut structured_storage,
     );
-    let mut storage_transaction = TxStorageTransaction::new(in_memory_transaction);
-    let er = block_executor.attempt_tx_execution_with_vm(
+    let mut storage_tx = TxStorageTransaction::new(in_memory_transaction);
+
+    let mut checked_tx = checked_tx;
+    if execution_options.extra_tx_checks {
+        checked_tx = block_executor.extra_tx_checks(checked_tx, header, &mut storage_tx, memory)?;
+    }
+
+    let exec_result = block_executor.attempt_tx_execution_with_vm(
         checked_tx,
         header,
         coinbase_contract_id,
         gas_price,
-        &mut storage_transaction,
+        &mut storage_tx,
         memory,
     )?;
     Ok((
-        er.0,
-        er.1,
-        er.2,
-        er.3,
-        storage_transaction.changes().clone(),
+        exec_result.0,
+        exec_result.1,
+        exec_result.2,
+        exec_result.3,
+        storage_tx.changes().clone(),
     ))
+}
+
+pub fn fvm_transact_commit<Tx, T>(
+    storage: &mut T,
+    checked_tx: Checked<Tx>,
+    header: &PartialBlockHeader,
+    coinbase_contract_id: ContractId,
+    gas_price: Word,
+    consensus_params: ConsensusParameters,
+    extra_tx_checks: bool,
+) -> Result<(bool, ProgramState, Tx, Vec<Receipt>, Changes)>
+where
+    Tx: ExecutableTransaction + Cacheable + Send + Sync + 'static,
+    <Tx as IntoChecked>::Metadata: CheckedMetadata + Send + Sync,
+    T: KeyValueMutate<Column = Column>,
+{
+    debug_log!("ecl(_fvm_transact_inner): start");
+
+    // TODO warmup storage from state based on tx inputs?
+    // let inputs = checked_tx.transaction().inputs();
+    // for input in inputs {
+    //     match input {
+    //         Input::CoinSigned(v) => {}
+    //         Input::CoinPredicate(v) => {}
+    //         Input::Contract(v) => {}
+    //         Input::MessageCoinSigned(v) => {}
+    //         Input::MessageCoinPredicate(v) => {}
+    //         Input::MessageDataSigned(v) => {}
+    //         Input::MessageDataPredicate(v) => {}
+    //     }
+    // }
+
+    let mut memory = MemoryInstance::new();
+    let res = fvm_transact(
+        storage,
+        checked_tx,
+        header,
+        coinbase_contract_id,
+        gas_price,
+        &mut memory,
+        consensus_params,
+        extra_tx_checks,
+    )?;
+
+    for (col_num, changes) in &res.4 {
+        let column: Column = col_num.clone().try_into().expect("valid column number");
+        match column {
+            Column::Metadata
+            | Column::ContractsRawCode
+            | Column::ContractsState
+            | Column::ContractsLatestUtxo
+            | Column::ContractsAssets
+            | Column::Coins => {
+                for (key, op) in changes {
+                    match op {
+                        WriteOperation::Insert(v) => {
+                            storage.write(key, column, v)?;
+                        }
+                        WriteOperation::Remove => {
+                            storage.delete(key, column)?;
+                        }
+                    }
+                }
+            }
+
+            Column::Transactions
+            | Column::FuelBlocks
+            | Column::FuelBlockMerkleData
+            | Column::FuelBlockMerkleMetadata
+            | Column::ContractsAssetsMerkleData
+            | Column::ContractsAssetsMerkleMetadata
+            | Column::ContractsStateMerkleData
+            | Column::ContractsStateMerkleMetadata
+            | Column::Messages
+            | Column::ProcessedTransactions
+            | Column::FuelBlockConsensus
+            | Column::ConsensusParametersVersions
+            | Column::StateTransitionBytecodeVersions
+            | Column::UploadedBytecodes
+            | Column::GenesisMetadata => {}
+        }
+    }
+
+    Ok(res)
 }
