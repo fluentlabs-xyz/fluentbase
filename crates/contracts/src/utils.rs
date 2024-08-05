b267a07e5cdb68c7c9052fc076805c235d75e92a
Stas Stepanov <stanislav@ankr.com>
2024-08-05 21:09:42 +0600

    cleanup

--- crates/contracts/src/utils.rs
+++ crates/contracts/src/utils.rs
@@ -27,32 +27,8 @@ use fuel_core_types::{
     },
     fuel_types,
 };
-// use fuel_tx::{
-//     consensus_parameters::{
-//         ConsensusParametersV1,
-//         ContractParametersV1,
-//         FeeParametersV1,
-//         PredicateParametersV1,
-//         ScriptParametersV1,
-//         TxParametersV1,
-//     },
-//     ConsensusParameters,
-//     ContractParameters,
-//     FeeParameters,
-//     GasCosts,
-//     PredicateParameters,
-//     ScriptParameters,
-//     TxParameters,
-// };
-// use fuel_vm::{
-//     checked_transaction::IntoChecked,
-//     fuel_types,
-//     fuel_types::AssetId,
-//     interpreter::CheckedMetadata,
-//     prelude::ExecutableTransaction,
-// };
 use revm::{
-    primitives::{hex::FromHex, SpecId, TransactTo, TxEnv},
+    primitives::{SpecId, TransactTo, TxEnv},
     Database,
     EvmBuilder,
 };
@@ -70,7 +46,6 @@ pub fn evm_builder_apply_envs<'a, CR: ContextReader, BuilderStage, EXT, DB: Data
     cr: &'a CR,
 ) -> EvmBuilder<'a, BuilderStage, EXT, DB> {
     builder
-        // .with_db(block_builder.db.take().unwrap())
         .with_spec_id(SpecId::CANCUN)
         .modify_block_env(|blk_env| {
             blk_env.number = U256::from(cr.block_number());
@@ -85,41 +60,6 @@ pub fn evm_builder_apply_envs<'a, CR: ContextReader, BuilderStage, EXT, DB: Data
             cfg_env.chain_id = cr.block_chain_id();
         })
 }
-
-// pub fn fuel_exec_tx<CR: ContextReader, Tx: ExecutableTransaction + IntoChecked>(
-//     cr: &CR,
-//     tx: Tx,
-//     interpreter_params: fuel_vm::interpreter::InterpreterParams,
-//     consensus_params: &fuel_tx::ConsensusParameters,
-// ) -> fuel_vm::state::StateTransition<Tx>
-// where
-//     <Tx as IntoChecked>::Metadata: CheckedMetadata,
-// {
-//     let tx_gas_price = cr.tx_gas_price().as_limbs()[0];
-//     let mut vm: fuel_vm::interpreter::Interpreter<_, _, _> =
-//         fuel_vm::interpreter::Interpreter::with_storage(
-//             fuel_vm::interpreter::MemoryInstance::new(),
-//             fuel_vm::storage::MemoryStorage::default(),
-//             interpreter_params,
-//         );
-//     let ready_tx = tx
-//         .into_checked(
-//             fuel_vm::fuel_types::BlockHeight::new(cr.block_number() as u32),
-//             consensus_params,
-//         )
-//         .expect("failed to convert tx into checked tx")
-//         .into_ready(
-//             tx_gas_price,
-//             consensus_params.gas_costs(),
-//             consensus_params.fee_params(),
-//         )
-//         .expect("failed to make tx ready");
-//     let vm_result: fuel_vm::state::StateTransition<_> = vm
-//         .transact(ready_tx)
-//         .expect("failed to exec transaction")
-//         .into();
-//     vm_result
-// }
 
 pub fn fuel_testnet_consensus_params<CR: ContextReader>(cr: &CR) -> ConsensusParameters {
     ConsensusParameters::V1(ConsensusParametersV1 {
