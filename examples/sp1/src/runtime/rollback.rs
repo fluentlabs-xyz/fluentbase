87a004f2a155043860ad46c27735136be673de60
easy <shinbogdan@gmail.com>
2024-09-27 23:46:09 +1000

    added bench tests with sp1 virtual machine

---
+++ examples/sp1/src/runtime/rollback.rs
@@ -0,0 +1,18 @@
+use crate::RuntimeContext;
+use fluentbase_types::{ExitCode, JournalCheckpoint};
+use rwasm::{core::Trap, Caller};
+
+pub struct SyscallRollback;
+
+impl SyscallRollback {
+    pub fn fn_handler(mut caller: Caller<'_, RuntimeContext>, checkpoint: u64) -> Result<(), Trap> {
+        Self::fn_impl(caller.data_mut(), JournalCheckpoint::from_u64(checkpoint))
+            .map_err(|err| err.into_trap())?;
+        Ok(())
+    }
+
+    pub fn fn_impl(ctx: &RuntimeContext, checkpoint: JournalCheckpoint) -> Result<(), ExitCode> {
+        ctx.jzkt().rollback(checkpoint);
+        Ok(())
+    }
+}
