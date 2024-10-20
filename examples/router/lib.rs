96cba3ec36394429dcff2a22c62f6d48a602aaf6
d1r1 <me@d1r1.me>
2024-10-20 22:44:48 +0200

    tests solidity mode works

--- examples/router/lib.rs
+++ examples/router/lib.rs
@@ -3,12 +3,12 @@ extern crate alloc;
 extern crate fluentbase_sdk;
 use alloc::string::String;
 use alloy_sol_types::SolCall;
-use bytes::{Buf, BytesMut};
+use bytes::{Buf, BufMut, BytesMut};
 use codec2::{encoder::SolidityABI, error::CodecError};
 use core::ops::Deref;
 use fluentbase_sdk::{
     basic_entrypoint,
-    derive::{router, signature, Contract},
+    derive::{router, Contract},
     Address,
     Bytes,
     SharedAPI,
@@ -24,11 +24,120 @@ pub trait RouterAPI {
     // fn custom_greeting(&self, message: Bytes) -> Bytes;
 }
 
-#[router(mode = "solidity")]
+// #[router(mode = "solidity")]
+// impl<SDK: SharedAPI> RouterAPI for ROUTER<SDK> {
+//     #[function_id("greeting(bytes)", validate(false))] // 0xf8194e48
+//     fn greeting(&self, message: Bytes, caller: Address) -> Bytes {
+//         message
+//     }
+// }
+
+// Recursive expansion of router macro
+// ====================================
+
+#[allow(unused_imports)]
+use fluentbase_sdk::derive::function_id;
 impl<SDK: SharedAPI> RouterAPI for ROUTER<SDK> {
-    #[signature("function greeting(bytes)")] // 0xf8194e48
     fn greeting(&self, message: Bytes, caller: Address) -> Bytes {
         message
+    }
+}
+
+pub use codec2::encoder::Encoder;
+pub type GreetingCallArgs = (Bytes, Address);
+pub struct GreetingCall(GreetingCallArgs);
+
+impl GreetingCall {
+    pub const SELECTOR: [u8; 4] = [34u8, 210u8, 183u8, 141u8];
+    pub const SIGNATURE: &'static str = "greeting(bytes,address)";
+    pub fn new(args: GreetingCallArgs) -> Self {
+        Self(args)
+    }
+    pub fn encode(&self) -> Bytes {
+        let mut buf = BytesMut::new();
+        SolidityABI::encode(&(self.0.clone().0, self.0.clone().1), &mut buf, 0).unwrap();
+        let encoded_args = buf.freeze();
+        let clean_args = if SolidityABI::<GreetingCallArgs>::is_dynamic() {
+            encoded_args[32..].to_vec()
+        } else {
+            encoded_args.to_vec()
+        };
+        Self::SELECTOR.iter().copied().chain(clean_args).collect()
+    }
+    pub fn decode(buf: &impl Buf) -> Result<Self, CodecError> {
+        println!("decode args");
+        let mut combined = BytesMut::new();
+
+        if SolidityABI::<GreetingCallArgs>::is_dynamic() {
+            combined.put_slice(&fluentbase_sdk::U256::from(32).to_be_bytes::<32>());
+        }
+
+        combined.put(buf.chunk());
+
+        let args = SolidityABI::<GreetingCallArgs>::decode(&combined, 0)?;
+        Ok(Self(args))
+    }
+}
+impl Deref for GreetingCall {
+    type Target = GreetingCallArgs;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+pub type GreetingCallTarget = <GreetingCall as Deref>::Target;
+pub type GreetingReturnArgs = (Bytes,);
+pub struct GreetingReturn(GreetingReturnArgs);
+
+impl GreetingReturn {
+    pub fn new(args: GreetingReturnArgs) -> Self {
+        Self(args)
+    }
+    pub fn encode(&self) -> Bytes {
+        let mut buf = BytesMut::new();
+        SolidityABI::encode(&self.0, &mut buf, 0).unwrap();
+        buf.freeze().into()
+    }
+    pub fn decode(buf: &impl Buf) -> Result<Self, CodecError> {
+        let args = SolidityABI::<GreetingReturnArgs>::decode(buf, 0)?;
+        Ok(Self(args))
+    }
+}
+impl Deref for GreetingReturn {
+    type Target = GreetingReturnArgs;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+pub type GreetingReturnTarget = <GreetingReturn as Deref>::Target;
+impl<SDK: SharedAPI> ROUTER<SDK> {
+    pub fn main(&mut self) {
+        let input_size = self.sdk.input_size();
+        if input_size < 4 {
+            {
+                panic!("input too short, cannot extract selector");
+            };
+        };
+        let mut full_input = fluentbase_sdk::alloc_slice(input_size as usize);
+        self.sdk.read(&mut full_input, 0);
+        let (selector, data_input) = full_input.split_at(4);
+        match [selector[0], selector[1], selector[2], selector[3]] {
+            GreetingCall::SELECTOR => {
+                let (message, caller) = match GreetingCall::decode(&data_input) {
+                    Ok(decoded) => (decoded.0 .0, decoded.0 .1),
+                    Err(err) => {
+                        {
+                            panic!("failed to decode input: {:?}", err);
+                        };
+                    }
+                };
+                let output = self.greeting(message, caller);
+                let encoded_output = GreetingReturn::new((output,)).encode();
+                self.sdk.write(&encoded_output);
+            }
+            _ => {
+                panic!("unknown method");
+            }
+        }
     }
 }
 
@@ -43,50 +152,8 @@ basic_entrypoint!(ROUTER);
 #[cfg(test)]
 mod tests {
     use super::*;
-    // TODO: we need to import it inside codec2 to avoid aditional import
-    use alloc::fmt;
     use alloy_sol_types::sol;
-    use bytes::BufMut;
     use fluentbase_sdk::{journal::JournalState, runtime::TestingContext, Bytes};
-    use hex_literal::hex;
-    use std::io::Read;
-
-    #[test]
-    fn test_encoding_decoding() {
-        let original = Bytes::from("Hello, World!!".as_bytes());
-        // let router = Bytes::
-        let mut buf = BytesMut::new();
-
-        SolidityABI::encode(&original, &mut buf, 0).unwrap();
-
-        let encoded = buf.freeze();
-        let expected_encoded = "0000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000000e48656c6c6f2c20576f726c642121000000000000000000000000000000000000";
-        println!("{:?}", hex::encode(&encoded));
-
-        assert_eq!(hex::encode(&encoded), expected_encoded);
-
-        println!("Decoding...");
-        let decoded = SolidityABI::<Bytes>::decode(&encoded, 0).unwrap();
-        println!("{:?}", decoded);
-        assert_eq!(decoded, original);
-    }
-    #[test]
-    fn test_encoding_decoding2() {
-        let encoded = hex::decode("f8194e480000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000040000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000000e48656c6c6f2c20576f726c642121000000000000000000000000000000000000").unwrap();
-        println!("{:?}", &encoded);
-        let encoded_bytes = Bytes::from(encoded);
-
-        let decoded_result =
-            SolidityABI::<<GreetingCall as Deref>::Target>::decode(&encoded_bytes, 4);
-        println!("decoded result: {:?}", decoded_result.is_ok());
-        let message = match decoded_result {
-            Ok(decoded) => decoded.0,
-            Err(_) => {
-                panic!("failed to decode input");
-            }
-        };
-        println!("message > ~ : {:?}", message);
-    }
 
     #[test]
     fn test_contract_works() {
@@ -96,38 +163,26 @@ mod tests {
         let greeting_call = GreetingCall::new((b.clone(), a.clone()));
 
         let input = greeting_call.encode();
+
+        // SOL INPUT
         sol!(
             function buying(bytes message, address caller);
         );
 
-        let buying_call = buyingCall {
+        let buying_call_sol = buyingCall {
             message: b.clone(),
             caller: a.clone(),
         };
 
-        let byuing_call_input = buying_call.abi_encode();
-        println!("bying_call_input: {:?}", hex::encode(&byuing_call_input));
+        let byuing_call_input_sol = buying_call_sol.abi_encode();
 
-        let expected_input = "0x00000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000040000000000000000000000000aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa000000000000000000000000000000000000000000000000000000000000000e48656c6c6f2c20576f726c642121000000000000000000000000000000000000";
-
-        println!("encoded: {:?}", hex::encode(&input));
-        assert_eq!(hex::encode(&input[4..]), expected_input);
-
-        // let expected_encoded =
-        // "0000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000000e48656c6c6f2c20576f726c642121000000000000000000000000000000000000"
-        // ;
-
-        // assert_eq!(hex::encode(&input[4..]), expected_encoded);
-
-        println!("Decoding...");
-
-        let decoded = SolidityABI::<Bytes>::decode(&input, 0).unwrap();
-
-        println!(
-            "decoded: {:?}",
-            String::from_utf8(decoded.to_vec()).unwrap()
+        assert_eq!(
+            hex::encode(&input[4..]),
+            hex::encode(&byuing_call_input_sol[4..])
         );
 
+        println!("Input: {:?}", hex::encode(&input));
+        println!("call contract...");
         let sdk = TestingContext::empty().with_input(input);
         let mut router = ROUTER::new(JournalState::empty(sdk.clone()));
         router.deploy();
@@ -135,8 +190,9 @@ mod tests {
 
         let encoded_output = &sdk.take_output();
 
-        let output = GreetingReturn::decode(&encoded_output.as_slice()).unwrap();
-        // TODO: uncomment next line
-        // assert_eq!(output.0 .0, msg);
+        let output =
+            SolidityABI::<GreetingReturnArgs>::decode(&encoded_output.as_slice(), 0).unwrap();
+
+        assert_eq!(output.0, b);
     }
 }
