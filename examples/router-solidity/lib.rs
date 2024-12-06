61288e20b574c9208f1a57d4f7ae2d6221b12202
d1r1 <me@d1r1.me>
2024-12-06 15:51:32 +0400

    feat(solidity-artifacts): add example of SolidityABI derive macro

--- examples/router-solidity/lib.rs
+++ examples/router-solidity/lib.rs
@@ -4,9 +4,17 @@ extern crate fluentbase_sdk;
 use alloc::string::String;
 use fluentbase_sdk::{
     basic_entrypoint,
-    derive::{function_id, router, Contract},
+    codec::Codec,
+    derive::{function_id, router, Contract, SolidityABI},
     SharedAPI,
+    U256,
 };
+
+#[derive(Codec, Debug, Clone, SolidityABI)]
+pub struct Point {
+    x: u64,
+    y: U256,
+}
 
 #[derive(Contract)]
 struct ROUTER<SDK> {
@@ -16,6 +24,7 @@ struct ROUTER<SDK> {
 pub trait RouterAPI {
     fn greeting(&self, message: String) -> String;
     fn custom_greeting(&self, message: String) -> String;
+    fn complex(&self, point: Point) -> Point;
 }
 
 #[router(mode = "solidity")]
@@ -28,6 +37,10 @@ impl<SDK: SharedAPI> RouterAPI for ROUTER<SDK> {
     #[function_id("customGreeting(string)")]
     fn custom_greeting(&self, message: String) -> String {
         message
+    }
+
+    fn complex(&self, point: Point) -> Point {
+        point
     }
 }
 
@@ -43,13 +56,11 @@ basic_entrypoint!(ROUTER);
 mod tests {
     use super::*;
     use alloy_sol_types::{sol, SolCall};
-    use fluentbase_sdk::{journal::JournalState, runtime::TestingContext, Address, Bytes};
+    use fluentbase_sdk::{journal::JournalState, runtime::TestingContext};
 
     #[test]
     fn test_contract_works() {
-        let b = Bytes::from("Hello, World!!".as_bytes());
         let s = String::from("Hello, World!!");
-        let a = Address::repeat_byte(0xAA);
 
         let greeting_call = GreetingCall::new((s.clone(),));
 
@@ -81,5 +92,44 @@ mod tests {
         let output = GreetingReturn::decode(&encoded_output.as_slice()).unwrap();
         println!("output: {:?}", &output.0);
         assert_eq!(output.0 .0, s);
+    }
+
+    #[test]
+    fn test_complex_works() {
+        let point = Point {
+            x: 42,
+            y: U256::from(100),
+        };
+
+        let complex_call = ComplexCall::new((point.clone(),));
+        let input = complex_call.encode();
+
+        sol!(
+            function complex((uint64,uint256) point);
+        );
+
+        let complex_call_sol = complexCall {
+            point: (point.x, point.y.into()),
+        };
+        let complex_call_input_sol = complex_call_sol.abi_encode();
+
+        assert_eq!(
+            hex::encode(&input[4..]),
+            hex::encode(&complex_call_input_sol[4..])
+        );
+
+        println!("Input: {:?}", hex::encode(&input));
+
+        let sdk = TestingContext::empty().with_input(input);
+        let mut router = ROUTER::new(JournalState::empty(sdk.clone()));
+        router.deploy();
+        router.main();
+
+        let encoded_output = &sdk.take_output();
+        println!("Output: {:?}", hex::encode(&encoded_output));
+        let output = ComplexReturn::decode(&encoded_output.as_slice()).unwrap();
+
+        assert_eq!(output.0 .0.x, point.x);
+        assert_eq!(output.0 .0.y, point.y);
     }
 }
