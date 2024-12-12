c0604a06929496488e17f1acb1cdb8c2806cea41
d1r1 <me@d1r1.me>
2024-12-12 13:40:16 +0400

    chore: restore shakmaty example - it's artifacts used in bench

---
+++ examples/shakmaty/lib.rs
@@ -0,0 +1,38 @@
+#![cfg_attr(target_arch = "wasm32", no_std)]
+extern crate fluentbase_sdk;
+
+use fluentbase_sdk::{basic_entrypoint, derive::Contract, SharedAPI};
+use shakmaty::{Chess, Position};
+
+#[derive(Contract)]
+struct SHAKMATY<SDK> {
+    #[allow(unused)]
+    sdk: SDK,
+}
+
+impl<SDK: SharedAPI> SHAKMATY<SDK> {
+    fn deploy(&self) {
+        // any custom deployment logic here
+    }
+    fn main(&self) {
+        let pos = Chess::default();
+        let legals = pos.legal_moves();
+        assert_eq!(legals.len(), 20);
+    }
+}
+
+basic_entrypoint!(SHAKMATY);
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use fluentbase_sdk::{journal::JournalState, runtime::TestingContext};
+
+    #[test]
+    fn test_contract_works() {
+        let sdk = JournalState::empty(TestingContext::empty());
+        let shakmaty = SHAKMATY::new(sdk);
+        shakmaty.deploy();
+        shakmaty.main();
+    }
+}
