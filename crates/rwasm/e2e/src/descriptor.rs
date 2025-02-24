83e3922e55b3e70262e3901b02b7a1a13ba8d8f4
Torprius <torrpriius@gmail.com>
2025-02-24 20:11:39 +0100

    fix typo descriptor.rs

--- crates/rwasm/e2e/src/descriptor.rs
+++ crates/rwasm/e2e/src/descriptor.rs
@@ -4,7 +4,7 @@ use std::{
 };
 use wast::token::Span;
 
-/// The desciptor of a Wasm spec test suite run.
+/// The descriptor of a Wasm spec test suite run.
 #[derive(Debug)]
 pub struct TestDescriptor {
     /// The path of the Wasm spec test `.wast` file.
