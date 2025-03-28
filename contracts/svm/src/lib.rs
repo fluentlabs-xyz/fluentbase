afa18ce1843ed9425e49145d5c52cfdcb08b3a35
Stas Stepanov <stanislav@ankr.com>
2025-03-28 12:20:09 +0700

    feat(svm): additional tests

--- contracts/svm/src/lib.rs
+++ contracts/svm/src/lib.rs
@@ -3,15 +3,116 @@ extern crate alloc;
 extern crate core;
 extern crate fluentbase_sdk;
 
-use fluentbase_sdk::{debug_log, func_entrypoint, ExitCode, SharedAPI};
+use alloc::{vec, vec::Vec};
+use fluentbase_sdk::{
+    debug_log,
+    func_entrypoint,
+    BlockContextReader,
+    Bytes,
+    ContractContextReader,
+    ExitCode,
+    SharedAPI,
+    TxContextReader,
+};
 use solana_ee_core::{
     account::ReadableAccount,
-    fluentbase_helpers::{exec_encoded_svm_batch_message, process_svm_result},
+    common::calculate_max_chunk_size,
+    fluentbase_helpers::{
+        exec_encoded_svm_batch_message,
+        exec_svm_batch_message,
+        process_svm_result,
+        BatchMessage,
+        SvmError,
+    },
     helpers::sdk_storage_read_account_data,
-    solana_program::system_program,
+    solana_program::{
+        bpf_loader_upgradeable,
+        bpf_loader_upgradeable::create_buffer,
+        message::Message,
+        pubkey::Pubkey,
+        system_program,
+    },
 };
 
-func_entrypoint!(main);
+func_entrypoint!(main, deploy);
+
+pub fn deploy(mut sdk: impl SharedAPI) {
+    // input must be solana elf bytes
+    let elf_program_bytes: Bytes = sdk.input().into();
+    let program_len = elf_program_bytes.len();
+    // TODO form deploy messages and exec them
+    // TODO form this Pubkeys:
+    //  pk_payer - caller
+    //  pk_exec - (becomes executable account key)
+    //  pk_buffer - just generate
+    //  pk_authority - (who can modify the program)
+    // debug_log!("elf_program_bytes.len {}", program_len);
+    // panic!("elf_program_bytes.len {}", program_len);
+
+    // TODO generate inter-dependant pubkey
+    let pk_payer = Pubkey::new_unique(); // must exist
+    let pk_exec = Pubkey::new_unique(); // may not exist
+    let pk_buffer = Pubkey::new_unique(); // must not exist
+    let pk_authority = Pubkey::new_unique(); // must exist
+
+    debug_log!("deploy.contract_caller {}", sdk.context().contract_caller());
+    debug_log!("deploy.block_coinbase {}", sdk.context().block_coinbase());
+    debug_log!(
+        "deploy.contract_address {}",
+        sdk.context().contract_address()
+    );
+    debug_log!("deploy.tx_origin {}", sdk.context().tx_origin());
+    debug_log!(
+        "deploy.contract_bytecode_address {}",
+        sdk.context().contract_bytecode_address()
+    );
+
+    let mut batch_message = BatchMessage::new(None);
+
+    let instructions = create_buffer(&pk_payer, &pk_buffer, &pk_authority, 0, program_len).unwrap();
+    let message = Message::new(&instructions, Some(&pk_payer));
+    batch_message.append_one(message);
+
+    let create_msg = |offset: u32, bytes: Vec<u8>| {
+        let instruction = bpf_loader_upgradeable::write(&pk_buffer, &pk_authority, offset, bytes);
+        let instructions = vec![instruction];
+        Message::new(&instructions, Some(&pk_payer))
+    };
+    let mut write_messages = vec![];
+    let chunk_size = calculate_max_chunk_size(&create_msg);
+    for (chunk, i) in elf_program_bytes.chunks(chunk_size).zip(0..) {
+        let offset = i * chunk_size;
+        let msg = create_msg(offset as u32, chunk.to_vec());
+        write_messages.push(msg);
+    }
+    batch_message.append_many(write_messages);
+
+    let instructions = bpf_loader_upgradeable::deploy_with_max_program_len(
+        &pk_payer,
+        &pk_exec,
+        &pk_buffer,
+        &pk_authority,
+        // TODO compute
+        10,
+        program_len,
+    )
+    .unwrap();
+    let message = Message::new(&instructions, Some(&pk_payer));
+    batch_message.append_one(message);
+
+    let result = exec_svm_batch_message(&mut sdk, batch_message);
+    let (output, exit_code) = process_svm_result(result);
+    if exit_code != ExitCode::Ok.into_i32() {
+        panic!(
+            "svm_exec error exit_code '{}' output '{:?}'",
+            exit_code,
+            output.as_ref()
+        );
+    }
+
+    let out = output.as_ref();
+    sdk.write(out);
+}
 
 pub fn main(mut sdk: impl SharedAPI) {
     let input = sdk.input();
@@ -56,6 +157,7 @@ mod tests {
     use solana_ee_core::{
         account::{AccountSharedData, ReadableAccount, WritableAccount},
         bincode,
+        common::calculate_max_chunk_size,
         fluentbase_helpers::BatchMessage,
         helpers::sdk_storage_write_account_data,
         native_loader,
@@ -102,10 +204,9 @@ mod tests {
         let pk_buffer = Pubkey::new_unique();
 
         let pk_exec = Pubkey::from([8; 32]);
-        let pk_exec_account = AccountSharedData::new(0, 0, &system_program_id);
 
-        let pk_9 = Pubkey::from([9; 32]);
-        let pk_9_account = AccountSharedData::new(100, 0, &system_program_id);
+        let pk_authority = Pubkey::from([9; 32]);
+        let pk_authority_account = AccountSharedData::new(100, 0, &system_program_id);
 
         let (pk_program_data, _) =
             Pubkey::find_program_address(&[pk_exec.as_ref()], &bpf_loader_upgradeable_id);
@@ -119,8 +220,6 @@ mod tests {
         let program_len = account_with_program.data().len();
 
         sdk_storage_write_account_data(&mut sdk, &pk_payer, &account_payer).unwrap();
-        sdk_storage_write_account_data(&mut sdk, &pk_9, &pk_9_account).unwrap();
-        sdk_storage_write_account_data(&mut sdk, &pk_exec, &pk_exec_account).unwrap();
         sdk_storage_write_account_data(&mut sdk, &pk_program_data, &pk_program_data_account)
             .unwrap();
         sdk_storage_write_account_data(
@@ -152,7 +251,35 @@ mod tests {
 
         let mut batch_message = BatchMessage::new(None);
 
-        let instructions = create_buffer(&pk_payer, &pk_buffer, &pk_9, 0, program_len).unwrap();
+        let instructions =
+            create_buffer(&pk_payer, &pk_buffer, &pk_authority, 0, program_len).unwrap();
+        let message = Message::new(&instructions, Some(&pk_payer));
+        batch_message.append_one(message);
+
+        let create_msg = |offset: u32, bytes: Vec<u8>| {
+            let instruction =
+                bpf_loader_upgradeable::write(&pk_buffer, &pk_authority, offset, bytes);
+            let instructions = vec![instruction];
+            Message::new(&instructions, Some(&pk_payer))
+        };
+        let mut write_messages = vec![];
+        let chunk_size = calculate_max_chunk_size(&create_msg);
+        for (chunk, i) in account_with_program.data().chunks(chunk_size).zip(0..) {
+            let offset = i * chunk_size;
+            let msg = create_msg(offset as u32, chunk.to_vec());
+            write_messages.push(msg);
+        }
+        batch_message.append_many(write_messages);
+
+        let instructions = bpf_loader_upgradeable::deploy_with_max_program_len(
+            &pk_payer,
+            &pk_exec,
+            &pk_buffer,
+            &pk_authority,
+            10,
+            account_with_program.data().len(),
+        )
+        .unwrap();
         let message = Message::new(&instructions, Some(&pk_payer));
         batch_message.append_one(message);
 
--- e2e/src/svm.rs
+++ e2e/src/svm.rs
@@ -1,6 +1,14 @@
 mod tests {
-    use crate::utils::{try_print_utf8_error, EvmTestingContext};
-    use fluentbase_sdk::{address, Address, Bytes, ContractContextV1, PRECOMPILE_SVM_RUNTIME};
+    use crate::utils::{EvmTestingContext, TxBuilder};
+    use fluentbase_sdk::{
+        address,
+        bytes,
+        Address,
+        Bytes,
+        ContractContextV1,
+        PRECOMPILE_SVM_RUNTIME,
+        U256,
+    };
     use hex_literal::hex;
     use revm::primitives::ExecutionResult;
     use solana_ee_core::{
@@ -36,7 +44,7 @@ mod tests {
     }
 
     #[test]
-    fn test_svm_deploy_exec() {
+    fn test_svm_exec_through_main_entry() {
         let mut ctx = EvmTestingContext::default();
         const DEPLOYER_ADDRESS: Address = address!("1231238908230948230948209348203984029834");
         ctx.sdk = ctx.sdk.with_contract_context(ContractContextV1 {
@@ -80,10 +88,14 @@ mod tests {
             &pk_bpf_loader_upgradeable,
             "../../solana-ee/crates/examples/hello-world/assets/solana_ee_hello_world.so",
         );
+        println!(
+            "elf file first 4 bytes: {:x?}",
+            &account_with_program.data()[..4]
+        );
 
         let program_len = account_with_program.data().len();
 
-        macro_rules! validated_stored_account {
+        macro_rules! validate_account_exists {
             ($core: tt) => {
                 paste::paste! {
                     let account_read = sdk_storage_read_account_data(&mut ctx.sdk, &[<pk_ $core>]).unwrap();
@@ -93,30 +105,30 @@ mod tests {
         }
 
         sdk_storage_write_account_data(&mut ctx.sdk, &pk_payer, &payer_account).unwrap();
-        validated_stored_account!(payer);
+        validate_account_exists!(payer);
         sdk_storage_write_account_data(&mut ctx.sdk, &pk_nine, &nine_account).unwrap();
-        validated_stored_account!(nine);
+        validate_account_exists!(nine);
         sdk_storage_write_account_data(&mut ctx.sdk, &pk_exec, &exec_account).unwrap();
-        validated_stored_account!(exec);
+        validate_account_exists!(exec);
         sdk_storage_write_account_data(&mut ctx.sdk, &pk_program_data, &program_data_account)
             .unwrap();
-        validated_stored_account!(program_data);
+        validate_account_exists!(program_data);
         sdk_storage_write_account_data(&mut ctx.sdk, &pk_system_program, &system_program_account)
             .unwrap();
-        validated_stored_account!(system_program);
+        validate_account_exists!(system_program);
         sdk_storage_write_account_data(
             &mut ctx.sdk,
             &pk_bpf_loader_upgradeable,
             &bpf_loader_upgradeable_account,
         )
         .unwrap();
-        validated_stored_account!(bpf_loader_upgradeable);
+        validate_account_exists!(bpf_loader_upgradeable);
         sdk_storage_write_account_data(&mut ctx.sdk, &pk_sysvar_clock, &sysvar_clock_account)
             .unwrap();
-        validated_stored_account!(sysvar_clock);
+        validate_account_exists!(sysvar_clock);
         sdk_storage_write_account_data(&mut ctx.sdk, &pk_sysvar_rent, &sysvar_rent_account)
             .unwrap();
-        validated_stored_account!(sysvar_rent);
+        validate_account_exists!(sysvar_rent);
         ctx.commit_storage();
 
         // init buffer, fill buffer, deploy
@@ -140,7 +152,7 @@ mod tests {
         //     write_messages.push(msg);
         // }
         // batch_message.append_many(write_messages);
-
+        //
         // let instructions = bpf_loader_upgradeable::deploy_with_max_program_len(
         //     &pk_payer,
         //     &pk_exec,
@@ -163,6 +175,111 @@ mod tests {
             Some(300_000_000),
             None,
         );
+        let output = result.output().unwrap_or_default();
+        assert!(result.is_success());
+        let expected_output = hex!("");
+        assert_eq!(hex::encode(expected_output), hex::encode(output));
+    }
+
+    #[test]
+    fn test_svm_deploy_exec() {
+        let mut ctx = EvmTestingContext::default();
+        const DEPLOYER_ADDRESS: Address = address!("1231238908230948230948209348203984029834");
+        ctx.sdk = ctx.sdk.with_contract_context(ContractContextV1 {
+            address: PRECOMPILE_SVM_RUNTIME,
+            ..Default::default()
+        });
+
+        // setup
+
+        let pk_system_program = system_program::id();
+        let pk_native_loader = native_loader::id();
+        let pk_bpf_loader_upgradeable = bpf_loader_upgradeable::id();
+        let pk_sysvar_clock = sysvar::clock::id();
+        let pk_sysvar_rent = sysvar::rent::id();
+
+        let system_program_account =
+            create_loadable_account_for_test("system_program_id", &pk_native_loader);
+        let bpf_loader_upgradeable_account =
+            create_loadable_account_for_test("bpf_loader_upgradeable_id", &pk_native_loader);
+        let sysvar_clock_account =
+            create_loadable_account_for_test("sysvar_clock_id", &pk_system_program);
+        let sysvar_rent_account =
+            create_loadable_account_for_test("sysvar_rent_id", &pk_system_program);
+
+        let pk_payer = Pubkey::new_unique();
+        let payer_account = AccountSharedData::new(100, 0, &pk_system_program);
+
+        let pk_buffer = Pubkey::new_unique();
+
+        let pk_exec = Pubkey::from([8; 32]);
+
+        let pk_nine = Pubkey::from([9; 32]);
+        let nine_account = AccountSharedData::new(100, 0, &pk_system_program);
+
+        let (pk_program_data, _) =
+            Pubkey::find_program_address(&[pk_exec.as_ref()], &pk_bpf_loader_upgradeable);
+        let program_data_account = AccountSharedData::new(0, 0, &pk_system_program);
+
+        let account_with_program = load_program_account_from_elf_file(
+            &pk_bpf_loader_upgradeable,
+            "../../solana-ee/crates/examples/hello-world/assets/solana_ee_hello_world.so",
+        );
+        let program_len = account_with_program.data().len();
+
+        macro_rules! validate_account_exists {
+            ($core: tt) => {
+                paste::paste! {
+                    let account_read = sdk_storage_read_account_data(&mut ctx.sdk, &[<pk_ $core>]).unwrap();
+                    assert_eq!(&account_read, &[<$core _account>]);
+                }
+            };
+        }
+
+        sdk_storage_write_account_data(&mut ctx.sdk, &pk_payer, &payer_account).unwrap();
+        validate_account_exists!(payer);
+        sdk_storage_write_account_data(&mut ctx.sdk, &pk_nine, &nine_account).unwrap();
+        validate_account_exists!(nine);
+        sdk_storage_write_account_data(&mut ctx.sdk, &pk_program_data, &program_data_account)
+            .unwrap();
+        validate_account_exists!(program_data);
+        sdk_storage_write_account_data(&mut ctx.sdk, &pk_system_program, &system_program_account)
+            .unwrap();
+        validate_account_exists!(system_program);
+        sdk_storage_write_account_data(
+            &mut ctx.sdk,
+            &pk_bpf_loader_upgradeable,
+            &bpf_loader_upgradeable_account,
+        )
+        .unwrap();
+        validate_account_exists!(bpf_loader_upgradeable);
+        sdk_storage_write_account_data(&mut ctx.sdk, &pk_sysvar_clock, &sysvar_clock_account)
+            .unwrap();
+        validate_account_exists!(sysvar_clock);
+        sdk_storage_write_account_data(&mut ctx.sdk, &pk_sysvar_rent, &sysvar_rent_account)
+            .unwrap();
+        validate_account_exists!(sysvar_rent);
+        ctx.commit_storage();
+
+        // init buffer, fill buffer, deploy
+
+        let program_bytes = account_with_program.data().to_vec();
+        let (contract_address, gas_used) =
+            ctx.deploy_evm_tx_with_gas(DEPLOYER_ADDRESS, program_bytes.into());
+
+        let mut batch_message = BatchMessage::new(None);
+
+        let instructions = create_buffer(&pk_payer, &pk_buffer, &pk_nine, 0, program_len).unwrap();
+        let message = Message::new(&instructions, Some(&pk_payer));
+        batch_message.append_one(message);
+
+        let input = bincode::serialize(&batch_message).unwrap();
+        let input: Bytes = input.into();
+        println!("input.len {} input '{:?}'", input.len(), input.as_ref());
+        ctx.add_balance(DEPLOYER_ADDRESS, U256::from(1e18));
+        let result = TxBuilder::call(&mut ctx, DEPLOYER_ADDRESS, contract_address, None)
+            .input(input)
+            .exec();
         let output = result.output().unwrap_or_default();
         assert!(result.is_success());
         let expected_output = hex!("");
