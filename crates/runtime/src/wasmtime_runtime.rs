eb94581a2169a6e5d3fd0ec74dce89f0250d3b80
Khasan Khafizov <xozzslip@gmail.com>
2025-03-28 15:36:32 +0400

    feat(wasmtime) resumable func execution via async execution

--- crates/runtime/src/wasmtime_runtime.rs
+++ crates/runtime/src/wasmtime_runtime.rs
@@ -6,14 +6,76 @@ use anyhow::Result;
 use core::{error::Error as StdError, fmt};
 use fluentbase_codec::{bytes::BytesMut, CompactABI};
 use fluentbase_types::{Bytes, FixedBytes, SyscallInvocationParams, B256};
-use wasmtime::{Caller, Config, Engine, Extern, Linker, Memory, Module, Store, Trap};
+use futures::{executor::block_on, task::noop_waker};
+use std::{
+    cell::RefCell,
+    collections::HashMap,
+    future::Future,
+    mem::drop,
+    pin::Pin,
+    task::{Context, Poll, RawWaker, RawWakerVTable, Waker},
+    time::Instant,
+};
+use wasmtime::{Caller, Config, Engine, Extern, Func, Linker, Memory, Module, Store, Trap};
 
-const MODULE: &str = "fluentbase_v1preview";
+struct AsyncExecutor {
+    store: Store,
+    func: Func,
+    future: Pin<Box<dyn Future<Output = Result<()>>>>,
+
+    waker: Waker,
+}
+
+impl AsyncExecutor {
+    fn new(store: Store, func: Func) -> Result<Self> {
+        // The code is not yet running after call_async(). Only future is created.
+        // Actual execution is happeinng when Future::poll() is executed.
+        let future = func.call_async(&mut store, ());
+        Self {
+            store: store,
+            func: main,
+            future: Box::pin(future),
+            waker: noop_waker(),
+        }
+    }
+
+    fn step(&self) {
+        let context = Context::from_waker(&self.waker);
+        let exit_code = match Future::poll(self.future, &mut context) {
+            Poll::Ready(value) => {
+                println!("Future completed with: {:?}", value);
+                0
+            }
+            Poll::Pending => {
+                unsafe {
+                    (*store_ref).fuel_async_yield_interval(None)?;
+                }
+                call_id as i32
+            }
+        };
+    }
+}
+
+pub struct CachingRuntime {
+    recoverable_runtimes: HashMap<u32>,
+    call_counter: u32,
+}
+
+impl CachingRuntime {
+    pub fn new() -> Self {
+        Self {
+            recoverable_runtimes: HashMap::new(),
+            call_counter: 0,
+        }
+    }
+}
+thread_local! {
+    static CACHING_RUNTIME: RefCell<CachingRuntime> = RefCell::new(CachingRuntime::new());
+}
 
 #[derive(Debug)]
 enum HostTermination {
     Exit,
-    Interrupt,
     MemoryOutOfBounds,
 }
 
@@ -63,14 +125,14 @@ mod builtins {
         }
     }
 
-    pub fn write(mut caller: Caller<'_, RuntimeContext>, offset: u32, length: u32) -> Result<()> {
+    fn write(mut caller: Caller<'_, RuntimeContext>, offset: u32, length: u32) -> Result<()> {
         let data = read_memory(&mut caller, offset, length)?;
         let context = caller.data_mut();
         context.execution_result.output.extend_from_slice(&data);
         Ok(())
     }
 
-    pub fn read(
+    fn read(
         mut caller: Caller<'_, RuntimeContext>,
         target_ptr: u32,
         offset: u32,
@@ -87,21 +149,22 @@ mod builtins {
         }
     }
 
-    pub fn input_size(caller: Caller<'_, RuntimeContext>) -> Result<u32> {
+    fn input_size(caller: Caller<'_, RuntimeContext>) -> Result<u32> {
         Ok(caller.data().input_size())
     }
 
-    pub fn exit(mut caller: Caller<'_, RuntimeContext>, exit_code: i32) -> Result<()> {
+    fn exit(mut caller: Caller<'_, RuntimeContext>, exit_code: i32) -> Result<()> {
         caller.data_mut().execution_result.exit_code = exit_code;
         Err(HostTermination::Exit.into())
     }
 
-    pub fn read_output(
+    fn read_output(
         mut caller: Caller<'_, RuntimeContext>,
         target_ptr: u32,
         offset: u32,
         length: u32,
     ) -> Result<()> {
+        println!("builtin::read_output()");
         let context = caller.data();
         let return_data = &context.execution_result.return_data;
         if offset + length <= return_data.len() as u32 {
@@ -114,12 +177,12 @@ mod builtins {
         }
     }
 
-    pub fn output_size(caller: Caller<'_, RuntimeContext>) -> Result<u32> {
+    fn output_size(caller: Caller<'_, RuntimeContext>) -> Result<u32> {
         let context = caller.data();
         Ok(context.execution_result.return_data.len() as u32)
     }
 
-    pub fn debug_log(
+    fn debug_log(
         mut caller: Caller<'_, RuntimeContext>,
         message_ptr: u32,
         message_length: u32,
@@ -129,7 +192,7 @@ mod builtins {
         Ok(())
     }
 
-    pub fn exec(
+    fn exec(
         mut caller: Caller<'_, RuntimeContext>,
         hash32_ptr: u32,
         input_ptr: u32,
@@ -137,6 +200,7 @@ mod builtins {
         fuel16_ptr: u32,
         state: u32,
     ) -> Result<i32> {
+        println!("builtin::exec()");
         let mut encoded_state = BytesMut::new();
         let fuel_limit = u64::MAX;
         let code_hash = read_memory(&mut caller, hash32_ptr, 32)?;
@@ -156,10 +220,12 @@ mod builtins {
         assert!(output.is_empty());
         output.extend(encoded_state.freeze().to_vec());
         //Err(HostTermination::Interrupt.into())
-        Err(Trap::Interrupt.into())
+        //Err(Trap::Interrupt.into())
+        caller.fuel_async_yield_interval(Some(1))?;
+        Ok(0)
     }
 
-    pub fn keccak256(
+    fn keccak256(
         mut caller: Caller<'_, RuntimeContext>,
         data_ptr: u32,
         data_len: u32,
@@ -171,53 +237,91 @@ mod builtins {
         Ok(())
     }
 
-    pub fn charge_fuel(mut caller: Caller<'_, RuntimeContext>, delta: u64) -> Result<u64> {
+    fn charge_fuel(mut caller: Caller<'_, RuntimeContext>, delta: u64) -> Result<u64> {
         Ok(u64::MAX)
     }
 
-    pub fn fuel(caller: Caller<'_, RuntimeContext>) -> Result<u64> {
+    fn fuel(caller: Caller<'_, RuntimeContext>) -> Result<u64> {
         Ok(u64::MAX)
+    }
+
+    pub fn linker() -> Result<Linker> {
+        let mut linker = Linker::new(&engine);
+        linker.func_wrap("fluentbase_v1preview", "_write", write)?;
+        linker.func_wrap("fluentbase_v1preview", "_read", read)?;
+        linker.func_wrap("fluentbase_v1preview", "_input_size", input_size)?;
+        linker.func_wrap("fluentbase_v1preview", "_exit", exit)?;
+        linker.func_wrap("fluentbase_v1preview", "_output_size", output_size)?;
+        linker.func_wrap("fluentbase_v1preview", "_read_output", read_output)?;
+        linker.func_wrap("fluentbase_v1preview", "_exec", exec)?;
+        linker.func_wrap("fluentbase_v1preview", "_debug_log", debug_log)?;
+        linker.func_wrap("fluentbase_v1preview", "_keccak256", keccak256)?;
+        linker.func_wrap("fluentbase_v1preview", "_fuel", fuel)?;
+        linker.func_wrap("fluentbase_v1preview", "_charge_fuel", charge_fuel)?;
+        Ok(linker)
     }
 }
 
-fn exec_internal(wasm_bytecode: &[u8], input: Vec<u8>) -> Result<(i32, Vec<u8>)> {
+fn resume_inner(call_id: u32, input: Vec<u8>) -> Result<(i32, Vec<u8>)> {
+    Ok((0, Vec::new()))
+}
+
+fn exec_inner(wasm_bytecode: &[u8], input: Vec<u8>) -> Result<(i32, Vec<u8>)> {
     let runtime_context = RuntimeContext::root(0).with_input(input);
     let mut config = Config::new();
     config.consume_fuel(true);
     config.async_support(true);
     let engine = Engine::new(&config)?;
     let module = Module::new(&engine, wasm_bytecode)?;
+    let linker = builtins::linker()?;
     let mut store = Store::new(&engine, runtime_context);
+    let store_ref = &mut store as *mut Store<RuntimeContext>;
+    store.set_fuel(u64::MAX);
+    store.fuel_async_yield_interval(None)?;
 
-    let mut linker = Linker::new(&engine);
-    linker.func_wrap(MODULE, "_write", builtins::write)?;
-    linker.func_wrap(MODULE, "_read", builtins::read)?;
-    linker.func_wrap(MODULE, "_input_size", builtins::input_size)?;
-    linker.func_wrap(MODULE, "_exit", builtins::exit)?;
-    linker.func_wrap(MODULE, "_output_size", builtins::output_size)?;
-    linker.func_wrap(MODULE, "_read_output", builtins::read_output)?;
-    linker.func_wrap(MODULE, "_exec", builtins::exec)?;
-    linker.func_wrap(MODULE, "_debug_log", builtins::debug_log)?;
-    linker.func_wrap(MODULE, "_keccak256", builtins::keccak256)?;
-    linker.func_wrap(MODULE, "_fuel", builtins::fuel)?;
-    linker.func_wrap(MODULE, "_charge_fuel", builtins::charge_fuel)?;
-
-    let instance = linker.instantiate(&mut store, &module)?;
+    let instance = block_on(linker.instantiate_async(&mut store, &module))?;
     let main = instance.get_typed_func::<(), ()>(&mut store, "main")?;
-    match main.call(&mut store, ()) {
-        Ok(_) => {}
-        Err(exit_code) => {
-            println!("hey here is error code {:?}", exit_code);
-            println!("{:?}", store.data().output());
-        }
-    }
-    main.call(&mut store, ());
 
-    return Ok((0, store.data().output().clone().into()));
+    let start = Instant::now();
+    let future = main.call_async(&mut store, ());
+    let mut pinned_future = Box::pin(future);
+
+    CACHING_RUNTIME.with_borrow_mut(|caching_runtime| {
+        caching_runtime.call_counter += 1;
+        let call_id = caching_runtime.call_counter;
+        caching_runtime
+            .recoverable_runtimes
+            .insert(call_id, pinned_future);
+        let future = caching_runtime
+            .recoverable_runtimes
+            .get_mut(&call_id)
+            .unwrap()
+            .as_mut();
+
+        let waker = dummy_waker();
+        let mut context = Context::from_waker(&waker);
+        let exit_code = match Future::poll(future, &mut context) {
+            Poll::Ready(value) => {
+                println!("Future completed with: {:?}", value);
+                0
+            }
+            Poll::Pending => {
+                unsafe {
+                    (*store_ref).fuel_async_yield_interval(None)?;
+                }
+                call_id as i32
+            }
+        };
+
+        drop(pinned_future);
+
+        println!("Main executed. Time elapsed is: {:?}", start.elapsed());
+        Ok((exit_code, store.data().output().clone().into()))
+    })
 }
 
 pub fn exec_in_wasmtime_runtime(wasm_bytecode: &[u8], input: Vec<u8>) -> (i32, Vec<u8>) {
-    exec_internal(wasm_bytecode, input).unwrap()
+    exec_inner(wasm_bytecode, input).unwrap()
 }
 
 #[cfg(test)]
