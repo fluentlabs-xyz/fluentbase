257d6d6b00cee9357ffaacc7df4cc700a8bacdaf
Khasan Khafizov <xozzslip@gmail.com>
2025-04-25 14:22:06 +0400

    test: add test for failing secp recover

--- crates/runtime/src/instruction/secp256k1_recover.rs
+++ crates/runtime/src/instruction/secp256k1_recover.rs
@@ -28,7 +28,10 @@ impl SyscallSecp256k1Recover {
     }
 
     pub fn fn_impl(digest: &B256, sig: &[u8; 64], rec_id: u8) -> Option<[u8; 65]> {
-        let recid = RecoveryId::from_i32(rec_id as i32).expect("recovery ID is valid");
+        let recid = match RecoveryId::from_i32(rec_id as i32) {
+            Ok(recid) => recid,
+            Err(_) => return None,
+        };
         let sig = match RecoverableSignature::from_compact(sig.as_slice(), recid) {
             Ok(sig) => sig,
             Err(_) => return None,
@@ -39,5 +42,82 @@ impl SyscallSecp256k1Recover {
             Err(_) => return None,
         };
         Some(public.serialize_uncompressed())
+    }
+}
+
+#[cfg(test)]
+mod secp256k1_tests {
+    extern crate alloc;
+
+    use crate::instruction::secp256k1_recover::SyscallSecp256k1Recover;
+    use fluentbase_types::B256;
+    use hex_literal::hex;
+    use k256::{elliptic_curve::sec1::ToEncodedPoint, PublicKey};
+    use sha2::{Digest, Sha256};
+
+    struct RecoveryTestVector {
+        pk: [u8; 33],
+        msg: &'static [u8],
+        sig: [u8; 65],
+        rec_id: u8,
+    }
+
+    const RECOVERY_TEST_VECTORS: &[RecoveryTestVector] = &[
+        // Recovery ID 0
+        RecoveryTestVector {
+            pk: hex!("021a7a569e91dbf60581509c7fc946d1003b60c7dee85299538db6353538d59574"),
+            msg: b"example message",
+            sig: hex!(
+                "04ce53abb3721bafc561408ce8ff99c909f7f0b18a2f788649d6470162ab1aa0323971edc523a6d6453f3fb6128d318d9db1a5ff3386feb1047d9816e780039d52"
+            ),
+            rec_id: 0,
+        },
+        // Recovery ID 1
+        RecoveryTestVector {
+            pk: hex!("036d6caac248af96f6afa7f904f550253a0f3ef3f5aa2fe6838a95b216691468e2"),
+            msg: b"example message",
+            sig: hex!(
+                "0446c05b6368a44b8810d79859441d819b8e7cdc8bfd371e35c53196f4bcacdb5135c7facce2a97b95eacba8a586d87b7958aaf8368ab29cee481f76e871dbd9cb"
+            ),
+            rec_id: 1,
+        },
+    ];
+
+    #[test]
+    fn public_key_recovery() {
+        for vector in RECOVERY_TEST_VECTORS {
+            let digest = Sha256::new_with_prefix(vector.msg).finalize();
+
+            let public_key = PublicKey::from_sec1_bytes(&vector.pk).unwrap();
+            let pk_uncompressed = public_key.to_encoded_point(false);
+            let mut pk = [0u8; 65];
+            pk.copy_from_slice(pk_uncompressed.as_bytes());
+
+            let result = SyscallSecp256k1Recover::fn_impl(
+                &B256::from_slice(&digest),
+                &vector.sig[1..].try_into().unwrap(),
+                vector.rec_id,
+            )
+            .unwrap();
+            assert_eq!(result, pk);
+        }
+    }
+
+    #[test]
+    fn public_key_recovery_failure() {
+        let vector = RecoveryTestVector {
+            pk: [0u8; 33],
+            msg: b"example message",
+            sig: [0u8; 65],
+            rec_id: 1,
+        };
+        let digest = Sha256::new_with_prefix(vector.msg).finalize();
+
+        let result = SyscallSecp256k1Recover::fn_impl(
+            &B256::from_slice(&digest),
+            &vector.sig[1..].try_into().unwrap(),
+            vector.rec_id,
+        );
+        assert!(result.is_none());
     }
 }
