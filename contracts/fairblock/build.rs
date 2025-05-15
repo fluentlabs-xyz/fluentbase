18178f20bfe99d4e77a8ef1212cd862b08f45d69
d1r1 <me@d1r1.me>
2025-05-15 18:59:56 +0400

    feat(fairblock): move under flag enable_go

--- contracts/fairblock/build.rs
+++ contracts/fairblock/build.rs
@@ -1,7 +1,15 @@
 fn main() {
-    let config = fluentbase_build::Config::default()
-        .with_rerun_if_changed("main.go")
-        .with_rerun_if_changed("go.mod")
-        .with_rerun_if_changed("go.sum");
-    fluentbase_build::compile_go_to_wasm(config)
+    #[cfg(feature = "enable_go")]
+    {
+        let config = fluentbase_build::Config::default()
+            .with_rerun_if_changed("main.go")
+            .with_rerun_if_changed("go.mod")
+            .with_rerun_if_changed("go.sum");
+        fluentbase_build::compile_go_to_wasm(config)
+    }
+
+    #[cfg(not(feature = "enable_go"))]
+    {
+        println!("cargo:warning=enable_go feature is not enabled, skipping Go compilation");
+    }
 }
--- contracts/fairblock/lib.rs
+++ contracts/fairblock/lib.rs
@@ -1,2 +1,5 @@
-#[cfg(not(target_arch = "wasm32"))]
+#[cfg(all(not(target_arch = "wasm32"), feature = "enable_go"))]
 pub const WASM_BYTECODE: &[u8] = fluentbase_sdk::include_this_wasm!();
+
+#[cfg(all(not(target_arch = "wasm32"), not(feature = "enable_go")))]
+pub const WASM_BYTECODE: &[u8] = &[];
