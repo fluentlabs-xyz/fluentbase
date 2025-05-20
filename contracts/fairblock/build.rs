cf21f1e7e831da92599a96bd0cf3341b4db4002d
Khasan Khafizov <xozzslip@gmail.com>
2025-05-20 14:49:43 +0400

    fix: use fallback.wasm absolute path

--- contracts/fairblock/build.rs
+++ contracts/fairblock/build.rs
@@ -1,4 +1,5 @@
 use fluentbase_build::{compile_go_to_wasm, is_tinygo_installed, Config};
+use std::fs;
 
 fn main() {
     if is_tinygo_installed() {
@@ -8,7 +9,11 @@ fn main() {
             .with_rerun_if_changed("go.sum");
         compile_go_to_wasm(config)
     } else {
-        println!("cargo:rustc-env=FLUENTBASE_WASM_ARTIFACT_PATH=fallback.wasm");
+        let path = fs::canonicalize("fallback.wasm").unwrap();
+        println!(
+            "cargo:rustc-env=FLUENTBASE_WASM_ARTIFACT_PATH={}",
+            path.to_str().unwrap()
+        );
         println!("cargo:warning=tinygo not found, wasm may be outdated");
     }
 }
