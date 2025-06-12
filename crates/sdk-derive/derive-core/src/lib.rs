19610a941d8c3574132ac16926b7362bc72631ab
d1r1 <me@d1r1.me>
2025-06-12 14:58:46 +0400

    feat(sdk-derive-core): make abi and method public

--- crates/sdk-derive/derive-core/src/lib.rs
+++ crates/sdk-derive/derive-core/src/lib.rs
@@ -1,11 +1,11 @@
 //! Core functionality for the router macro implementation.
 //! This crate provides the base functionality used by the proc-macro crate.
 
-mod abi;
+pub mod abi;
 pub mod attr;
 pub mod client;
 mod codec;
-mod method;
+pub mod method;
 pub mod router;
 mod signature;
 pub mod sol_input;
