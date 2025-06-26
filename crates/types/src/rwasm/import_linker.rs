81a3b4a3a0d2d0995ab0f62bad736e5dfcde84af
Khasan Khafizov <xozzslip@gmail.com>
2025-06-26 20:24:11 +0400

    fix: fix linker after merge

--- crates/types/src/rwasm/import_linker.rs
+++ crates/types/src/rwasm/import_linker.rs
@@ -1,129 +1,52 @@
-use crate::{rwasm::block_fuel::MINIMAL_BASE_FUEL_COST, SysFuncIdx};
+use crate::{emit_fuel_procedure, SysFuncIdx};
 use alloc::rc::Rc;
-use rwasm::{instruction_set, ImportLinker, ImportName, ValType};
+use rwasm::{ImportLinker, ImportName, ValType};
 
 pub fn create_import_linker() -> Rc<ImportLinker> {
     let mut import_linker = ImportLinker::default();
     macro_rules! import_function {
-        ($func_name:literal, $sys_func_idx:ident, $params:expr, $results:expr, $fuel_expr:ident) => {
+        ($func_name:literal, $sys_func_idx:ident, $params:expr, $results:expr) => {
             import_linker.insert_function(
                 ImportName::new("fluentbase_v1preview", $func_name),
                 SysFuncIdx::$sys_func_idx as u32,
-                instruction_set! {
-                    .op_consume_fuel(MINIMAL_BASE_FUEL_COST as u32)
-                },
+                emit_fuel_procedure(SysFuncIdx::$sys_func_idx),
                 $params,
                 $results,
             );
         };
     }
-    import_function!(
-        "_keccak256",
-        KECCAK256,
-        &[ValType::I32; 3],
-        &[],
-        KECCAK256_FUEL
-    );
-    import_function!("_exit", EXIT, &[ValType::I32; 1], &[], EXIT_FUEL);
-    import_function!("_state", STATE, &[], &[ValType::I32; 1], STATE_FUEL);
-    import_function!(
-        "_read",
-        READ_INPUT,
-        &[ValType::I32; 3],
-        &[],
-        READ_INPUT_FUEL
-    );
-    import_function!(
-        "_input_size",
-        INPUT_SIZE,
-        &[],
-        &[ValType::I32; 1],
-        INPUT_SIZE_FUEL
-    );
-    import_function!(
-        "_write",
-        WRITE_OUTPUT,
-        &[ValType::I32; 2],
-        &[],
-        WRITE_OUTPUT_FUEL
-    );
-    import_function!(
-        "_output_size",
-        OUTPUT_SIZE,
-        &[],
-        &[ValType::I32; 1],
-        OUTPUT_SIZE_FUEL
-    );
-    import_function!(
-        "_read_output",
-        READ_OUTPUT,
-        &[ValType::I32; 3],
-        &[],
-        READ_OUTPUT_FUEL
-    );
-    import_function!(
-        "_exec",
-        EXEC,
-        &[ValType::I32; 5],
-        &[ValType::I32; 1],
-        EXEC_FUEL
-    );
-    import_function!(
-        "_resume",
-        RESUME,
-        &[ValType::I32; 5],
-        &[ValType::I32; 1],
-        RESUME_FUEL
-    );
-    import_function!(
-        "_forward_output",
-        FORWARD_OUTPUT,
-        &[ValType::I32; 2],
-        &[],
-        FORWARD_OUTPUT_FUEL
-    );
+    import_function!("_keccak256", KECCAK256, &[ValType::I32; 3], &[]);
+    import_function!("_exit", EXIT, &[ValType::I32; 1], &[]);
+    import_function!("_state", STATE, &[], &[ValType::I32; 1]);
+    import_function!("_read", READ_INPUT, &[ValType::I32; 3], &[]);
+    import_function!("_input_size", INPUT_SIZE, &[], &[ValType::I32; 1]);
+    import_function!("_write", WRITE_OUTPUT, &[ValType::I32; 2], &[]);
+    import_function!("_output_size", OUTPUT_SIZE, &[], &[ValType::I32; 1]);
+    import_function!("_read_output", READ_OUTPUT, &[ValType::I32; 3], &[]);
+    import_function!("_exec", EXEC, &[ValType::I32; 5], &[ValType::I32; 1]);
+    import_function!("_resume", RESUME, &[ValType::I32; 5], &[ValType::I32; 1]);
+    import_function!("_forward_output", FORWARD_OUTPUT, &[ValType::I32; 2], &[]);
     import_function!(
         "_charge_fuel_manually",
         CHARGE_FUEL_MANUALLY,
         &[ValType::I64; 2],
-        &[ValType::I64; 1],
-        CHARGE_FUEL_MANUALLY_FUEL
+        &[ValType::I64; 1]
     );
-    import_function!(
-        "_charge_fuel",
-        CHARGE_FUEL,
-        &[ValType::I64; 1],
-        &[],
-        CHARGE_FUEL_FUEL
-    );
-    import_function!("_fuel", FUEL, &[], &[ValType::I64; 1], FUEL_FUEL);
+    import_function!("_charge_fuel", CHARGE_FUEL, &[ValType::I64; 1], &[]);
+    import_function!("_fuel", FUEL, &[], &[ValType::I64; 1]);
     import_function!(
         "_preimage_size",
         PREIMAGE_SIZE,
         &[ValType::I32; 1],
-        &[ValType::I32; 1],
-        PREIMAGE_SIZE_FUEL
+        &[ValType::I32; 1]
     );
-    import_function!(
-        "_preimage_copy",
-        PREIMAGE_COPY,
-        &[ValType::I32; 2],
-        &[],
-        PREIMAGE_COPY_FUEL
-    );
-    import_function!(
-        "_debug_log",
-        DEBUG_LOG,
-        &[ValType::I32; 2],
-        &[],
-        DEBUG_LOG_FUEL
-    );
+    import_function!("_preimage_copy", PREIMAGE_COPY, &[ValType::I32; 2], &[]);
+    import_function!("_debug_log", DEBUG_LOG, &[ValType::I32; 2], &[]);
     import_function!(
         "_secp256k1_recover",
         SECP256K1_RECOVER,
         &[ValType::I32; 4],
-        &[ValType::I32; 1],
-        SECP256K1_RECOVER_FUEL
+        &[ValType::I32; 1]
     );
     Rc::new(import_linker)
 }
