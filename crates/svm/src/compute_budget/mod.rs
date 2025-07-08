c886033038a6688323278ef20670a66c38c3bf7b
bfdays <stanislav@fluentlabs.xyz>
2025-07-08 12:03:12 +0700

    feat(svm): get rid of upgradable loader

--- crates/svm/src/compute_budget/mod.rs
+++ crates/svm/src/compute_budget/mod.rs
@@ -1,4 +1,2 @@
 pub mod compute_budget;
 pub mod compute_budget_limits;
-// pub mod fee;
-// pub mod prioritization_fee;
--- crates/svm/src/solana_program/address_lookup_table/error.rs
+++ crates/svm/src/solana_program/address_lookup_table/error.rs
@@ -3,7 +3,6 @@
 #[derive(Debug, /*Error, */ PartialEq, Eq, Clone)]
 pub enum AddressLookupError {
     /// Attempted to lookup addresses from a table that does not exist
-    // #[error("Attempted to lookup addresses from a table that does not exist")]
     LookupTableAccountNotFound,
 
     /// Attempted to lookup addresses from an account owned by the wrong program
