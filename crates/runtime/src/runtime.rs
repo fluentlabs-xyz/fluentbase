87702177705dd6017f022fdafed396e32087e663
d1r1 <me@d1r1.me>
2025-08-01 13:02:18 +0400

    fix(runtime): restore inter-process lock during WASM module compilation

--- crates/runtime/src/runtime.rs
+++ crates/runtime/src/runtime.rs
@@ -6,28 +6,13 @@ use crate::{
 use fluentbase_codec::{bytes::BytesMut, CompactABI};
 use fluentbase_types::{
     byteorder::{ByteOrder, LittleEndian},
-    create_import_linker,
-    Address,
-    BytecodeOrHash,
-    Bytes,
-    ExitCode,
-    SysFuncIdx,
-    B256,
-    STATE_DEPLOY,
+    create_import_linker, Address, BytecodeOrHash, Bytes, ExitCode, SysFuncIdx, B256, STATE_DEPLOY,
     STATE_MAIN,
 };
 use hashbrown::{hash_map::Entry, HashMap};
 use rwasm::{
-    ExecutionEngine,
-    ExecutorConfig,
-    ImportLinker,
-    RwasmModule,
-    Store,
-    Strategy,
-    TrapCode,
-    TypedCaller,
-    TypedStore,
-    Value,
+    ExecutionEngine, ExecutorConfig, ImportLinker, RwasmModule, Store, Strategy, TrapCode,
+    TypedCaller, TypedStore, Value,
 };
 use std::{cell::RefCell, fmt::Debug, mem::take, rc::Rc, sync::Arc};
 
@@ -81,9 +66,15 @@ impl CachingRuntime {
         let rwasm_module = Rc::new(RwasmModule::new_or_empty(rwasm_bytecode.as_ref()).0);
         #[cfg(feature = "wasmtime")]
         if fluentbase_types::is_system_precompile(&address) {
-            let config = fluentbase_types::default_compilation_config();
-            let wasmtime_module =
-                rwasm::compile_wasmtime_module(config, &rwasm_module.wasm_section).unwrap();
+            let wasmtime_module = {
+                let lock =
+                    InterProcessLock::acquire_on_b256(FILE_NAME_PREFIX1, &code_hash).unwrap();
+                let config = fluentbase_types::default_compilation_config();
+                let wasmtime_module =
+                    rwasm::compile_wasmtime_module(config, &rwasm_module.wasm_section).unwrap();
+                drop(lock);
+                wasmtime_module
+            };
             let strategy = Arc::new(Strategy::Wasmtime {
                 module: Rc::new(wasmtime_module),
                 resumable: fluentbase_types::is_resumable_precompile(&address),
