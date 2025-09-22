72477bf614cdf933890c4a5ebb7d4b6786876541
Dmitry Savonin <nwtsoas@gmail.com>
2025-09-22 13:28:58 +0300

    fix(runtime): add retention for cached stores, add warming up wasmtime modules though build scripts for e2e tests
    Signed-off-by: Dmitry Savonin <nwtsoas@gmail.com>

--- evm-e2e/src/utils.rs
+++ evm-e2e/src/utils.rs
@@ -1,8 +1,14 @@
 use crate::runner::execute_test_suite;
+use fluentbase_runtime::Runtime;
+use fluentbase_sdk::{
+    b256, keccak256,
+    rwasm_core::{deserialize_wasmtime_module, CompilationConfig, Strategy},
+};
 use k256::ecdsa::SigningKey;
 use revm::primitives::Address;
 use std::{
     path::Path,
+    rc::Rc,
     sync::{Arc, Mutex},
     time::Duration,
 };
@@ -18,6 +24,28 @@ pub(crate) fn run_e2e_test(test_path: &'static str) {
     let path = format!("./{}", test_path);
     let elapsed = Arc::new(Mutex::new(Duration::new(0, 0)));
     execute_test_suite(Path::new(path.as_str()), &elapsed, false, false).unwrap();
+}
+
+mod precompiled {
+    include!(concat!(env!("OUT_DIR"), "/precompiled_module.rs"));
+}
+
+#[ctor::ctor]
+fn warmup_wasmtime_modules() {
+    let wasmtime_module = deserialize_wasmtime_module(
+        CompilationConfig::default(),
+        &precompiled::PRECOMPILED_RUNTIME_EVM_CWASM_MODULE,
+    )
+    .expect("failed to parse wasmtime module");
+    let code_hash = keccak256(&precompiled::PRECOMPILED_RUNTIME_EVM_RWASM_MODULE);
+    println!("precompiled evm runtime code hash: {:?}", code_hash);
+    // 0xc62b88c3b842aea2c89fb1a69212d8e24925936873c4f52c38c4496eb6c491b2
+    Runtime::warmup_strategy_raw(
+        b256!("0xc62b88c3b842aea2c89fb1a69212d8e24925936873c4f52c38c4496eb6c491b2"),
+        Strategy::Wasmtime {
+            module: Rc::new(wasmtime_module),
+        },
+    );
 }
 
 #[cfg(test)]
