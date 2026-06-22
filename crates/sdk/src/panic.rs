ccf9c8fd33520c76c77f9e4c52f6b241f5fa5ca0
Dmitry Savonin <github@dmitry123.xyz>
2026-06-22 11:45:39 +0400

    feat(sdk): evm compatible panic messages
    Signed-off-by: Dmitry Savonin <github@dmitry123.xyz>

--- crates/sdk/src/panic.rs
+++ crates/sdk/src/panic.rs
@@ -1,22 +1,25 @@
-#[cfg(target_arch = "wasm32")]
+use crate::evm::write_evm_panic_message;
+use fluentbase_types::{ExitCode, NativeAPI, RwasmContext};
+
 #[cfg(not(feature = "fast-panic"))]
 pub unsafe fn handle_panic_info(info: &core::panic::PanicInfo) -> ! {
-    use fluentbase_types::{ExitCode, NativeAPI, RwasmContext};
     let panic_message = alloc::format!("{}", info.message());
     let native_sdk = RwasmContext {};
-    native_sdk.write(panic_message.as_bytes());
+    write_evm_panic_message(panic_message.as_str(), |message_bytes| {
+        native_sdk.write(message_bytes)
+    });
     native_sdk.exit(ExitCode::Panic)
 }
 
-#[cfg(target_arch = "wasm32")]
 #[cfg(feature = "fast-panic")]
 pub unsafe fn handle_panic_info(info: &core::panic::PanicInfo) -> ! {
-    use fluentbase_types::{ExitCode, NativeAPI, RwasmContext};
     let Some(message) = info.message().as_str() else {
-        // TODO(dmitry123): "how to support multiline panic messages?"
+        // TODO(dmitry123): How to support multiline panic messages? Not supported API by Rust yet w/o alloc.
         unreachable!("multiline or panic with args is not supported with fast-panic feature")
     };
     let native_sdk = RwasmContext {};
-    native_sdk.write(message.as_bytes());
+    write_evm_panic_message(panic_message.as_str(), |message_bytes| {
+        native_sdk.write(message_bytes)
+    });
     native_sdk.exit(ExitCode::Panic)
 }
