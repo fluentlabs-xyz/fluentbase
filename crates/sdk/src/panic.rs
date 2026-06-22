d74140e773e542e37dc78f3c0defdd07b5b819ee
Hedwig <hedwig0x@users.noreply.github.com>
2026-06-22 08:05:30 +0000

    fix(sdk): encode wasm panics as evm errors

--- crates/sdk/src/panic.rs
+++ crates/sdk/src/panic.rs
@@ -1,22 +1,26 @@
 #[cfg(target_arch = "wasm32")]
 #[cfg(not(feature = "fast-panic"))]
 pub unsafe fn handle_panic_info(info: &core::panic::PanicInfo) -> ! {
+    use crate::evm::write_evm_panic_message;
     use fluentbase_types::{ExitCode, NativeAPI, RwasmContext};
+
     let panic_message = alloc::format!("{}", info.message());
     let native_sdk = RwasmContext {};
-    native_sdk.write(panic_message.as_bytes());
+    write_evm_panic_message(&panic_message, |slice| native_sdk.write(slice));
     native_sdk.exit(ExitCode::Panic)
 }
 
 #[cfg(target_arch = "wasm32")]
 #[cfg(feature = "fast-panic")]
 pub unsafe fn handle_panic_info(info: &core::panic::PanicInfo) -> ! {
+    use crate::evm::write_evm_panic_message;
     use fluentbase_types::{ExitCode, NativeAPI, RwasmContext};
+
     let Some(message) = info.message().as_str() else {
         // TODO(dmitry123): "how to support multiline panic messages?"
         unreachable!("multiline or panic with args is not supported with fast-panic feature")
     };
     let native_sdk = RwasmContext {};
-    native_sdk.write(message.as_bytes());
+    write_evm_panic_message(message, |slice| native_sdk.write(slice));
     native_sdk.exit(ExitCode::Panic)
 }
